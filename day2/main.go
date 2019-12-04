7717edc491d24fa5c765d37a06eb1d506ec6486f
James Bogosian <bogosj@gmail.com>
2019-12-04 18:19:54 -0500

    add day 2 solution

---
+++ day2/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
+
+func compute(in []int, n, v int) int {
+	p := in
+	p[1] = n
+	p[2] = v
+	s := 0
+	for {
+		f := p[s : s+4]
+		op := f[0]
+		if op == 1 {
+			p[f[3]] = p[f[1]] + p[f[2]]
+		} else if op == 2 {
+			p[f[3]] = p[f[1]] * p[f[2]]
+		} else if op == 99 {
+			break
+		}
+		s += 4
+	}
+	return p[0]
+}
+
+func main() {
+	fmt.Printf("result: %v\n", compute(input(), 12, 2))
+
+	for n := 0; n < 100; n++ {
+		for v := 0; v < 100; v++ {
+			if compute(input(), n, v) == 19690720 {
+				fmt.Printf("n=%v v=%v, a=%v\n", n, v, 100*n+v)
+			}
+		}
+	}
+}
+
+func input() []int {
+	var ret []int
+	for _, v := range strings.Split(rawinput(), ",") {
+		iv, _ := strconv.Atoi(v)
+		ret = append(ret, iv)
+	}
+	return ret
+}
+
+func rawinput() string {
+	data, _ := ioutil.ReadFile("input.txt")
+	return string(data)
+}
