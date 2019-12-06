131111c4b08d0b7d80da617c4c06f167804d046c
James Bogosian <bogosj@gmail.com>
2019-12-06 15:37:54 -0500

    Day 6 part one solved.

--- day6/main.go
+++ day6/main.go
@@ -6,8 +6,45 @@ import (
 	"strings"
 )
 
+type object struct {
+	name   string
+	orbits *object
+}
+
+func (o *object) NumOrbits() int {
+	if o.orbits == nil {
+		return 0
+	}
+	return o.orbits.NumOrbits() + 1
+}
+
+func getObj(objs map[string]*object, name string) *object {
+	obj, ok := objs[name]
+	if !ok {
+		o := object{name: name}
+		obj = &o
+		objs[name] = obj
+	}
+	return obj
+}
+
+func establishOrbit(objs map[string]*object, name1, name2 string) {
+	obj1 := getObj(objs, name1)
+	obj2 := getObj(objs, name2)
+	obj2.orbits = obj1
+}
+
 func main() {
-	fmt.Println(input())
+	objs := map[string]*object{}
+	for _, pair := range input() {
+		establishOrbit(objs, pair[0], pair[1])
+	}
+
+	totalOrbits := 0
+	for _, v := range objs {
+		totalOrbits += v.NumOrbits()
+	}
+	fmt.Println(totalOrbits)
 }
 
 func input() [][]string {
