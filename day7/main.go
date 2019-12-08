6f8084de6d7fdf18595fe2188dda2dbee617a185
James Bogosian <bogosj@gmail.com>
2019-12-07 22:20:00 -0500

    complete day 7

--- day7/main.go
+++ day7/main.go
@@ -8,19 +8,65 @@ import (
 	"strings"
 )
 
-func main() {
+func part1() {
 	maxOutput := math.MinInt64
 	allPhases := permutations([]int{0, 1, 2, 3, 4})
 	for _, phase := range allPhases {
 		ampIn := 0
 		for _, i := range phase {
-			ampIn = compute(input(), []int{i, ampIn})
+			c := computer{prog: input()}
+			out, err := c.compute([]int{i, ampIn})
+			if err != nil {
+				fmt.Println("err:", err)
+			}
+			ampIn = out
 		}
 		if ampIn > maxOutput {
 			maxOutput = ampIn
 		}
 	}
 	fmt.Println(maxOutput)
+}
+
+func part2() {
+	maxOutput := math.MinInt64
+	allPhases := permutations([]int{5, 6, 7, 8, 9})
+	for _, phase := range allPhases {
+		computers := make([]computer, 5)
+		ampIn := 0
+		for i := 0; i < len(computers); i++ {
+			computers[i].prog = input()
+		}
+		i := 0
+		maxEOutput := 0
+		for {
+			in := []int{ampIn}
+			if len(phase) > 0 {
+				in = append([]int{phase[0]}, in...)
+				phase = phase[1:]
+			}
+			out, err := computers[i%len(computers)].compute(in)
+			if err != nil {
+				break
+			}
+			if i%len(computers) == 4 {
+				if out > maxEOutput {
+					maxEOutput = out
+				}
+			}
+			ampIn = out
+			i++
+		}
+		if maxEOutput > maxOutput {
+			maxOutput = maxEOutput
+		}
+	}
+	fmt.Println(maxOutput)
+}
+
+func main() {
+	part1()
+	part2()
 }
 
 func input() []int {
