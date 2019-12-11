fcf12c2410328198a7da8a0d6d2180aeef2b1ad0
James Bogosian <bogosj@gmail.com>
2019-12-10 19:09:33 -0500

    Add timing to day 3

--- 2019/day3/main.go
+++ 2019/day3/main.go
@@ -7,6 +7,7 @@ import (
 	"sort"
 	"strconv"
 	"strings"
+	"time"
 )
 
 // Point defines a point in cartesian space.
@@ -101,9 +102,12 @@ func main() {
 		w2.move(move)
 	}
 	points := w1.intersects(w2)
+	start := time.Now()
 	closestPointManhattan(points)
+	fmt.Println("part 1 done in:", time.Since(start))
+	start = time.Now()
 	closestPointOnWires(points, w1, w2)
-	// TODO eval path to all crosspoints
+	fmt.Println("part 2 done in:", time.Since(start))
 }
 
 func input() [][]string {
@@ -112,20 +116,6 @@ func input() [][]string {
 	w1 := strings.Split(lines[0], `,`)
 	w2 := strings.Split(lines[1], `,`)
 	return [][]string{w1, w2}
-}
-
-func sample1() string {
-	return `R8,U5,L5,D3
-U7,R6,D4,L4`
-}
-
-func sample2() string {
-	return `R75,D30,R83,U83,L12,D49,R71,U7,L72
-U62,R66,U55,R34,D71,R55,D58,R83`
-}
-func sample3() string {
-	return `R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
-U98,R91,D20,R16,D67,R40,U7,R15,U6,R7`
 }
 
 func rawinput() string {
