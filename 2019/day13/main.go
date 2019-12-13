8e906c074ff00a6ff14947d478611dc4d211ab16
James Bogosian <bogosj@gmail.com>
2019-12-13 10:45:11 -0500

    Skeleton code for day 13

---
+++ 2019/day13/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func part1() {
+}
+
+func part2() {
+}
+
+func main() {
+	start := time.Now()
+	part1()
+	fmt.Println("Part 1 done in:", time.Since(start))
+	start = time.Now()
+	part2()
+	fmt.Println("Part 2 done in:", time.Since(start))
+}
+
+func input(n string) map[int]int {
+	ret := map[int]int{}
+	lines := strings.Split(rawinput(n), "\n")
+	for i, v := range strings.Split(lines[0], ",") {
+		iv, err := strconv.Atoi(v)
+		if err != nil {
+			fmt.Println(err)
+		}
+		ret[i] = iv
+	}
+	return ret
+}
+
+func rawinput(n string) string {
+	data, _ := ioutil.ReadFile(n)
+	return string(data)
+}
