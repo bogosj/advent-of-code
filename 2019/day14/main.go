9787a2811f07376003d2e34aae47f8219ca3703b
James Bogosian <bogosj@gmail.com>
2019-12-14 18:42:27 -0500

    Day 14 complete

---
+++ 2019/day14/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+	"time"
+
+	"jamesbogosian.com/advent-of-code/2019/day14/factory"
+)
+
+func part1() {
+	f := factory.New("input.txt")
+	start := time.Now()
+	fmt.Println(f.Ore(1))
+	fmt.Println("took:", time.Since(start))
+}
+
+func part2() {
+	f := factory.New("input.txt")
+	start := time.Now()
+	ore := 1000000000000
+	fuel := sort.Search(ore, func(n int) bool {
+		return f.Ore(n) > ore
+	}) - 1
+	fmt.Printf("Can make %d fuel with %d ore\n", fuel, ore)
+	fmt.Println("took:", time.Since(start))
+}
+
+func main() {
+	part1()
+	part2()
+}
