127a0472397d66cca56ae7c9102d6e9dc905f692
James Bogosian <bogosj@gmail.com>
2019-12-13 23:34:10 -0500

    refactor

--- 2019/day11/main.go
+++ 2019/day11/main.go
@@ -2,16 +2,14 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"time"
+
 	"jamesbogosian.com/advent-of-code/2019/computer"
 	"jamesbogosian.com/advent-of-code/2019/day11/robot"
-	"strconv"
-	"strings"
-	"time"
 )
 
 func part1() {
-	c := computer.New(input("input.txt"))
+	c := computer.New("input.txt")
 	r := robot.New(c)
 	r.Paint(0)
 	panels := r.PrintHull()
@@ -19,7 +17,7 @@ func part1() {
 }
 
 func part2() {
-	c := computer.New(input("input.txt"))
+	c := computer.New("input.txt")
 	r := robot.New(c)
 	r.Paint(1)
 	r.PrintHull()
@@ -32,22 +30,4 @@ func main() {
 	start = time.Now()
 	part2()
 	fmt.Println("Part 2 done in:", time.Since(start))
-}
-
-func input(n string) map[int]int {
-	ret := map[int]int{}
-	lines := strings.Split(rawinput(n), "\n")
-	for i, v := range strings.Split(lines[0], ",") {
-		iv, err := strconv.Atoi(v)
-		if err != nil {
-			fmt.Println(err)
-		}
-		ret[i] = iv
-	}
-	return ret
-}
-
-func rawinput(n string) string {
-	data, _ := ioutil.ReadFile(n)
-	return string(data)
 }
