df0ded32ab2bda9b85b750fe152b3b70ccaaec98
James Bogosian <bogosj@gmail.com>
2019-12-18 20:52:14 -0500

    Solved day 18 part 1

--- 2019/intmath/intmath.go
+++ 2019/intmath/intmath.go
@@ -19,10 +19,42 @@ type Bitmap struct {
 	b uint64
 }
 
-// Set sets the n-th bit to true.
+// String returns a string representation of the Bitmap.
+func (b Bitmap) String() string {
+	return string(b.b)
+}
+
+// Unset sets the n-th bit to 0.
+func (b Bitmap) Unset(i int) Bitmap {
+	b.b &^= 1 << i
+	return b
+}
+
+// Set sets the n-th bit to 1.
 func (b Bitmap) Set(i int) Bitmap {
 	b.b |= 1 << i
 	return b
+}
+
+// OneBits returns the cardinal value of all bits set to 1.
+func (b Bitmap) OneBits() (ret []int) {
+	for i := 0; i < 64; i++ {
+		if b.b&1 == 1 {
+			ret = append(ret, i)
+		}
+		b.b = b.b >> 1
+	}
+	return
+}
+
+// AndEq bitwise ANDs the values and returns true if the result matches the called Bitmap.
+func (b Bitmap) AndEq(o Bitmap) bool {
+	return b.b&o.b == b.b
+}
+
+// Val exposes the underlying int value of the Bitmap.
+func (b Bitmap) Val() uint64 {
+	return b.b
 }
 
 // Gcd returns the greatest common denominator of two numbers.
