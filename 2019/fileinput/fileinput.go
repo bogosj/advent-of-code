2e6ea040d8b4381d9f47edd7ca86b70b24afbaad
James Bogosian <bogosj@gmail.com>
2019-12-23 03:46:43 +0000

    Add a raw input method

--- 2019/fileinput/fileinput.go
+++ 2019/fileinput/fileinput.go
@@ -11,3 +11,9 @@ func ReadLines(n string) []string {
 	full := strings.TrimSpace(string(data))
 	return strings.Split(full, "\n")
 }
+
+// ReadLinesRaw accepts a file path and returns a slice of strings of lines in the file without trimming any space.
+func ReadLinesRaw(n string) []string {
+	data, _ := ioutil.ReadFile(n)
+	return strings.Split(string(data), "\n")
+}
