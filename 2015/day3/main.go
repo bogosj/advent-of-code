9dd0311d09840f8d905c0151d96ad5fcd5045341
James Bogosian <bogosj@gmail.com>
2019-12-29 16:09:26 +0000

    Day 3 complete.

--- 2015/day3/main.go
+++ 2015/day3/main.go
@@ -3,12 +3,55 @@ package main
 import (
 	"fmt"
 	"time"
+
+	"github.com/bogosj/advent-of-code/intmath"
+
+	"github.com/bogosj/advent-of-code/fileinput"
 )
 
+func input() string {
+	lines := fileinput.ReadLines("input.txt")
+	return lines[0]
+}
+
 func part1() {
+	m := map[intmath.Point]bool{}
+	p := intmath.Point{}
+	for _, r := range input() {
+		m[p] = true
+		switch r {
+		case '^':
+			p.Y--
+		case '>':
+			p.X++
+		case '<':
+			p.X--
+		case 'v':
+			p.Y++
+		}
+	}
+	m[p] = true
+	fmt.Println("Houses visited:", len(m))
 }
 
 func part2() {
+	m := map[intmath.Point]bool{}
+	p := []intmath.Point{intmath.Point{}, intmath.Point{}}
+	for i, r := range input() {
+		m[p[i%2]] = true
+		switch r {
+		case '^':
+			p[i%2].Y--
+		case '>':
+			p[i%2].X++
+		case '<':
+			p[i%2].X--
+		case 'v':
+			p[i%2].Y++
+		}
+		m[p[i%2]] = true
+	}
+	fmt.Println("Houses visited:", len(m))
 }
 
 func main() {
