a254a71ed85d6c56f5f6d36d73b56e4d82c1a2c9
James Bogosian <bogosj@gmail.com>
2019-12-29 17:10:10 +0000

    Day 5 part 1 complete.

---
+++ 2015/day5/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/bogosj/advent-of-code/fileinput"
+)
+
+/*
+It contains at least three vowels (aeiou only), like aei, xazegov, or aeiouaeiouaeiou.
+It contains at least one letter that appears twice in a row, like xx, abcdde (dd), or aabbccdd (aa, bb, cc, or dd).
+It does not contain the strings ab, cd, pq, or xy, even if they are part of one of the other requirements.
+*/
+var (
+	vowels = map[rune]bool{'a': true, 'e': true, 'i': true, 'o': true, 'u': true}
+)
+
+func hasThreeVowels(s string) bool {
+	i := 0
+	for _, r := range s {
+		if vowels[r] == true {
+			i++
+		}
+		if i == 3 {
+			return true
+		}
+	}
+	return false
+}
+
+func hasDupe(s string) bool {
+	for i := 1; i < len(s); i++ {
+		if s[i] == s[i-1] {
+			return true
+		}
+	}
+	return false
+}
+
+func hasBadString(s string) bool {
+	for _, ss := range []string{"ab", "cd", "pq", "xy"} {
+		if strings.Contains(s, ss) {
+			return true
+		}
+	}
+	return false
+}
+
+func nice(s string) bool {
+	return hasThreeVowels(s) && hasDupe(s) && !hasBadString(s)
+}
+
+func part1() {
+	lines := fileinput.ReadLines("input.txt")
+	i := 0
+	for _, line := range lines {
+		if nice(line) {
+			i++
+		}
+	}
+	fmt.Println("Nice strings:", i)
+}
+
+func part2() {
+}
+
+func main() {
+	start := time.Now()
+	part1()
+	fmt.Println("Part 1 done in:", time.Since(start))
+	start = time.Now()
+	part2()
+	fmt.Println("Part 2 done in:", time.Since(start))
+}
