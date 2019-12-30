6d93b4de4bbf2c6762886a1cca761fc5d1ac4619
James Bogosian <bogosj@gmail.com>
2019-12-29 22:52:09 -0500

    Refactor permutations up to intmath

--- 2019/day7/main.go
+++ 2019/day7/main.go
@@ -6,23 +6,11 @@ import (
 	"time"
 
 	"github.com/bogosj/advent-of-code/2019/computer"
+	"github.com/bogosj/advent-of-code/intmath"
 )
 
 func permutations(in []int) (p [][]int) {
-	var rc func([]int, int)
-	rc = func(a []int, k int) {
-		if k == len(a) {
-			p = append(p, append([]int{}, a...))
-		} else {
-			for i := k; i < len(in); i++ {
-				a[k], a[i] = a[i], a[k]
-				rc(a, k+1)
-				a[k], a[i] = a[i], a[k]
-			}
-		}
-	}
-	rc(in, 0)
-	return p
+	return intmath.Permutations(in)
 }
 
 func part1() {
---
+++ intmath/permutations.go
@@ -0,0 +1,19 @@
+package intmath
+
+// Permutations returns all of the permutations of a given slice of ints.
+func Permutations(in []int) (p [][]int) {
+	var rc func([]int, int)
+	rc = func(a []int, k int) {
+		if k == len(a) {
+			p = append(p, append([]int{}, a...))
+		} else {
+			for i := k; i < len(in); i++ {
+				a[k], a[i] = a[i], a[k]
+				rc(a, k+1)
+				a[k], a[i] = a[i], a[k]
+			}
+		}
+	}
+	rc(in, 0)
+	return p
+}
