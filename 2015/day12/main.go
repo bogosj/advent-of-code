fedcce08cfbef287fa8a22e998bd40620b630979
James Bogosian <bogosj@gmail.com>
2019-12-31 10:41:35 -0500

    Day 12 done.

--- 2015/day12/main.go
+++ 2015/day12/main.go
@@ -8,24 +8,33 @@ import (
 	"github.com/bogosj/advent-of-code/fileinput"
 )
 
-func processMap(m map[string]interface{}, f *float64) {
+func processMap(m map[string]interface{}, f *float64, ignoreRed bool) {
+	if ignoreRed {
+		for _, v := range m {
+			if s, ok := v.(string); ok {
+				if s == "red" {
+					return
+				}
+			}
+		}
+	}
 	for _, v := range m {
-		processUnknown(v, f)
+		processUnknown(v, f, ignoreRed)
 	}
 }
 
-func processSlice(d []interface{}, f *float64) {
+func processSlice(d []interface{}, f *float64, ignoreRed bool) {
 	for _, dd := range d {
-		processUnknown(dd, f)
+		processUnknown(dd, f, ignoreRed)
 	}
 }
 
-func processUnknown(d interface{}, f *float64) {
+func processUnknown(d interface{}, f *float64, ignoreRed bool) {
 	if s, ok := d.([]interface{}); ok {
-		processSlice(s, f)
+		processSlice(s, f, ignoreRed)
 	}
 	if m, ok := d.(map[string]interface{}); ok {
-		processMap(m, f)
+		processMap(m, f, ignoreRed)
 	}
 	if fv, ok := d.(float64); ok {
 		*f += fv
@@ -40,11 +49,20 @@ func part1() {
 	if err != nil {
 		panic(err)
 	}
-	processSlice(d.([]interface{}), &f)
+	processSlice(d.([]interface{}), &f, false)
 	fmt.Println("Sum of all numbers:", f)
 }
 
 func part2() {
+	lines := fileinput.ReadLines("input.txt")
+	var d interface{}
+	var f float64
+	err := json.Unmarshal([]byte(lines[0]), &d)
+	if err != nil {
+		panic(err)
+	}
+	processSlice(d.([]interface{}), &f, true)
+	fmt.Println("Sum of all numbers:", f)
 }
 
 func main() {
