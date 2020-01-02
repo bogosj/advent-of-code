e7bd73c4ca0b6ebc50b211b001234ba42dfa811b
James Bogosian <bogosj@gmail.com>
2020-01-02 15:38:48 -0500

    Day 19 part 1 complete.

---
+++ 2015/day19/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"github.com/bogosj/advent-of-code/2015/day19/medicine"
+	"time"
+)
+
+func part1() {
+	m := medicine.New("input.txt")
+	c := m.Calibrate()
+	fmt.Println("Machine calibration:", c)
+}
+
+func part2() {
+}
+
+func main() {
+	start := time.Now()
+	part1()
+	fmt.Println("Part 1 done in:", time.Since(start))
+	start = time.Now()
+	part2()
+	fmt.Println("Part 2 done in:", time.Since(start))
+}
