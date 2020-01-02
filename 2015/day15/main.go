93dfc80fadd2d10211ae319264ee7084d567730e
James Bogosian <bogosj@gmail.com>
2020-01-01 19:24:25 -0500

    wip

--- 2015/day15/main.go
+++ 2015/day15/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"strings"
 	"time"
@@ -8,6 +9,29 @@ import (
 	"github.com/bogosj/advent-of-code/fileinput"
 	"github.com/bogosj/advent-of-code/intmath"
 )
+
+var (
+	attrs = strings.Split("capacity,durability,flavor,texture", ",")
+)
+
+func score(m map[string]map[string]int, a, b, c, d int) (int, error) {
+	if a+b+c+d > 100 {
+		return 0, errors.New("too many ingredients")
+	}
+	ret := 1
+	for _, attr := range attrs {
+		valA := m["Butterscotch"][attr] * a
+		valB := m["Candy"][attr] * b
+		valC := m["Chogolate"][attr] * c
+		valD := m["Sprinkles"][attr] * d
+		val := valA + valB + valC + valD
+		if val < 0 {
+			return 0, errors.New("negative value")
+		}
+		ret *= val
+	}
+	return ret, nil
+}
 
 func input(p string) map[string]map[string]int {
 	ret := map[string]map[string]int{}
@@ -29,7 +53,21 @@ func input(p string) map[string]map[string]int {
 }
 
 func part1() {
-	fmt.Println(input("input.txt"))
+	ing := input("input.txt")
+	bestScore := 0
+	for a := 0; a <= 100; a++ {
+		for b := 0; b <= 100; b++ {
+			for c := 0; c <= 100; c++ {
+				for d := 0; d <= 0; d++ {
+					i, err := score(ing, a, b, c, d)
+					if err != nil && i> bestScore {
+						bestScore = i
+					}
+				}
+			}
+		}
+	}
+	fmt.Println("Best score is:", bestScore)
 }
 
 func part2() {
