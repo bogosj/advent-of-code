d49f7dfdfa83618b588855fbc6504fedf84b5000
James Bogosian <bogosj@gmail.com>
2020-01-05 19:55:45 -0500

    Day 7 complete

--- 2016/day7/main.go
+++ 2016/day7/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"regexp"
+	"strings"
 	"time"
 
 	"github.com/bogosj/advent-of-code/fileinput"
@@ -18,11 +19,11 @@ func input() []string {
 }
 
 func containsABBA(s string) bool {
-	for len(s)>=4 {
-		if s[0]==s[3] && s[1]==s[2] && s[0]!=s[1]{
+	for len(s) >= 4 {
+		if s[0] == s[3] && s[1] == s[2] && s[0] != s[1] {
 			return true
 		}
-		s=s[1:]
+		s = s[1:]
 	}
 	return false
 }
@@ -31,7 +32,7 @@ func supportsTLS(s string) bool {
 	if !containsABBA(re.ReplaceAllString(s, "*")) {
 		return false
 	}
-	for _, match:=range re.FindAllStringSubmatch(s, -1) {
+	for _, match := range re.FindAllStringSubmatch(s, -1) {
 		if containsABBA(match[1]) {
 			return false
 		}
@@ -39,10 +40,33 @@ func supportsTLS(s string) bool {
 	return true
 }
 
+func findABAs(s string) (ret []string) {
+	for len(s) >= 3 {
+		if s[0] == s[2] && s[1] != s[2] {
+			ret = append(ret, s[0:3])
+		}
+		s = s[1:]
+	}
+	return
+}
+
+func supportsSSL(s string) bool {
+	abas := findABAs(re.ReplaceAllString(s, "*"))
+	for _, match := range re.FindAllStringSubmatch(s, -1) {
+		for _, aba := range abas {
+			bab := string(aba[1]) + string(aba[0]) + string(aba[1])
+			if strings.Contains(match[1], bab) {
+				return true
+			}
+		}
+	}
+	return false
+}
+
 func part1() {
 	in := input()
-	c:=0
-	for _, line:=range in {
+	c := 0
+	for _, line := range in {
 		if supportsTLS(line) {
 			c++
 		}
@@ -51,6 +75,14 @@ func part1() {
 }
 
 func part2() {
+	in := input()
+	c := 0
+	for _, line := range in {
+		if supportsSSL(line) {
+			c++
+		}
+	}
+	fmt.Println(c, "IPs support SSL")
 }
 
 func main() {
