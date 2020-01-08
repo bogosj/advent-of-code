73a56044610a2a282e5c46c636d8f415c67370a7
James Bogosian <bogosj@gmail.com>
2020-01-08 15:20:46 -0500

    Day 14 done

--- 2016/day14/main_test.go
+++ 2016/day14/main_test.go
@@ -1,6 +1,8 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func Test_substrToFind(t *testing.T) {
 	type args struct {
@@ -40,6 +42,38 @@ func Test_substrToFind(t *testing.T) {
 			}
 			if got != tt.want {
 				t.Errorf("substrToFind() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_getMd5(t *testing.T) {
+	type args struct {
+		s       string
+		i       int
+		stretch bool
+		cache   map[int]string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "abc0 - normal",
+			args: args{s: "abc", i: 0, stretch: false, cache: map[int]string{}},
+			want: "577571be4de9dcce85a041ba0410f29f",
+		},
+		{
+			name: "abc0 - stretched",
+			args: args{s: "abc", i: 0, stretch: true, cache: map[int]string{}},
+			want: "a107ff634856bb300138cac6568c0f24",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := getMd5(tt.args.s, tt.args.i, tt.args.stretch, tt.args.cache); got != tt.want {
+				t.Errorf("getMd5() = %v, want %v", got, tt.want)
 			}
 		})
 	}
