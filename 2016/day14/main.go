d5abe77cb79285ab8c07d6d42acf4df248d2ae53
James Bogosian <bogosj@gmail.com>
2020-01-08 11:40:38 -0500

    Day 14 start

---
+++ 2016/day14/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+const (
+	input = "ngcjuoqr"
+)
+
+func part1() {
+}
+
+func part2() {
+}
+
+func main() {
+	start := time.Now()
+	part1()
+	fmt.Println("Part 1 done in:", time.Since(start))
+	start = time.Now()
+	part2()
+	fmt.Println("Part 2 done in:", time.Since(start))
+}
