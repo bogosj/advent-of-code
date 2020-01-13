da0e29f05e6d3cc7df2a6c798e270091d5a956d6
James Bogosian <bogosj@gmail.com>
2020-01-13 12:33:31 -0500

    Add a Manhattan distance function to intmath.Point

--- intmath/point_test.go
+++ intmath/point_test.go
@@ -38,3 +38,55 @@ func TestPoint_Neighbors(t *testing.T) {
 		})
 	}
 }
+
+func TestPoint_ManhattanDistanceTo(t *testing.T) {
+	type fields struct {
+		X int
+		Y int
+	}
+	type args struct {
+		op Point
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   int
+	}{
+		{
+			name:   "basic",
+			fields: fields{X: 0, Y: 0},
+			args:   args{op: Point{X: 1, Y: 1}},
+			want:   2,
+		},
+		{
+			name:   "both positive",
+			fields: fields{X: 5, Y: 5},
+			args:   args{op: Point{X: 1, Y: 1}},
+			want:   8,
+		},
+		{
+			name:   "both negative",
+			fields: fields{X: -5, Y: -5},
+			args:   args{op: Point{X: -1, Y: -1}},
+			want:   8,
+		},
+		{
+			name:   "one negative",
+			fields: fields{X: -1, Y: -2},
+			args:   args{op: Point{X: 1, Y: 1}},
+			want:   5,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			p := Point{
+				X: tt.fields.X,
+				Y: tt.fields.Y,
+			}
+			if got := p.ManhattanDistanceTo(tt.args.op); got != tt.want {
+				t.Errorf("Point.ManhattanDistanceTo() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
