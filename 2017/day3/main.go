2c85e15dd74fea2330623aa2a295e873880d0817
James Bogosian <bogosj@gmail.com>
2020-01-13 12:39:19 -0500

    Day 3 complete

--- 2017/day3/main.go
+++ 2017/day3/main.go
@@ -83,6 +83,85 @@ func makeMem() map[intmath.Point]int {
 	}
 }
 
+func sumNeighbors(p intmath.Point, mem map[intmath.Point]int) (ret int) {
+	for _, n := range p.AllNeighbors() {
+		if v, ok := mem[n]; ok {
+			ret += v
+		}
+	}
+	if ret == 0 {
+		ret = 1
+	}
+	return
+}
+
+func makeMemSquares() int {
+	mem := map[intmath.Point]int{}
+	var cur, next intmath.Point
+	for {
+		//move down until nothight right
+		for {
+			mem[cur] = sumNeighbors(cur, mem)
+			if mem[cur] > input {
+				return mem[cur]
+			}
+			next = cur
+			next.X++
+			_, ok := mem[next]
+			if !ok {
+				break
+			}
+			cur.Y++
+		}
+		cur = next
+		//move right until nothing above
+		for {
+			mem[cur] = sumNeighbors(cur, mem)
+			if mem[cur] > input {
+				return mem[cur]
+			}
+			next = cur
+			next.Y--
+			_, ok := mem[next]
+			if !ok {
+				break
+			}
+			cur.X++
+		}
+		cur = next
+		//move up until nothing left
+		for {
+			mem[cur] = sumNeighbors(cur, mem)
+			if mem[cur] > input {
+				return mem[cur]
+			}
+			next = cur
+			next.X--
+			_, ok := mem[next]
+			if !ok {
+				break
+			}
+			cur.Y--
+		}
+		cur = next
+		//move left until nothing below
+		for {
+			mem[cur] = sumNeighbors(cur, mem)
+			if mem[cur] > input {
+				return mem[cur]
+			}
+			next = cur
+			next.Y++
+			_, ok := mem[next]
+			if !ok {
+				break
+			}
+			cur.X--
+		}
+		cur = next
+	}
+}
+
 func part1() {
 	for k, v := range makeMem() {
 		if v == input {
@@ -92,6 +171,7 @@ func part1() {
 }
 
 func part2() {
+	fmt.Println(makeMemSquares(), "is the first value larger than the input")
 }
 
 func main() {
