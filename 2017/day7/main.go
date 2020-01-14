cb472454508cacd632b4b2e58552afb29421004d
James Bogosian <bogosj@gmail.com>
2020-01-13 21:12:34 -0500

    Day 7 done

--- 2017/day7/main.go
+++ 2017/day7/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sort"
 	"strings"
 	"time"
 
@@ -17,10 +18,35 @@ func input() (ret [][]string) {
 }
 
 type program struct {
-	name   string
-	weight int
-	data   []string
-	parent *program
+	name     string
+	weight   int
+	data     []string
+	parent   *program
+	children []*program
+}
+
+func (p *program) String() string {
+	return fmt.Sprintf("%s (%d|%d)", p.name, p.weight, p.fullWeight())
+}
+
+func (p *program) fullWeight() int {
+	ret := p.weight
+	for _, child := range p.children {
+		ret += child.fullWeight()
+	}
+	return ret
+}
+
+func (p *program) printUnbalanced() {
+	sort.Slice(p.children, func(i, j int) bool { return p.children[i].fullWeight() < p.children[j].fullWeight() })
+	fmt.Printf("%v - %v\n", p, p.children)
+	if len(p.children) > 1 {
+		if p.children[0].fullWeight() != p.children[1].fullWeight() {
+			p.children[0].printUnbalanced()
+		} else {
+			p.children[len(p.children)-1].printUnbalanced()
+		}
+	}
 }
 
 func allPrograms() map[string]*program {
@@ -47,6 +73,11 @@ func makeTree(m map[string]*program) {
 			}
 		}
 	}
+	for _, v := range m {
+		if v.parent != nil {
+			v.parent.children = append(v.parent.children, v)
+		}
+	}
 }
 
 func part1() {
@@ -63,6 +94,9 @@ func part1() {
 }
 
 func part2() {
+	a := allPrograms()
+	makeTree(a)
+	a["mkxke"].printUnbalanced()
 }
 
 func main() {
