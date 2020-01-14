5ed99cc6dcb208f80de440cdaa35a9d5f8575b0f
James Bogosian <bogosj@gmail.com>
2020-01-14 10:57:16 -0500

    Day 10 start

---
+++ 2017/day10/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+var (
+	input = []int{183, 0, 31, 146, 254, 240, 223, 150, 2, 206, 161, 1, 255, 232, 199, 88}
+)
+
+func part1() {
+}
+
+func part2() {
+}
+
+func main() {
+	start := time.Now()
+	part1()
+	fmt.Println("Part 1 done in:", time.Since(start))
+	start = time.Now()
+	part2()
+	fmt.Println("Part 2 done in:", time.Since(start))
+}
