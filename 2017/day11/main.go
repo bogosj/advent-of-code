52f5fdaf1ccc4f3cbec844e2308940b15fc2dca6
James Bogosian <bogosj@gmail.com>
2020-01-14 17:01:09 -0500

    Day 11 done

--- 2017/day11/main.go
+++ 2017/day11/main.go
@@ -19,20 +19,30 @@ func countSteps(in []string) int {
 	for _, dir := range in {
 		s[dir]++
 	}
-	fmt.Println(s)
 	for _, pair := range [][]string{{"n", "s"}, {"ne", "sw"}, {"nw", "se"}} {
 		min := intmath.Min(s[pair[0]], s[pair[1]])
 		s[pair[0]] -= min
 		s[pair[1]] -= min
 	}
-	fmt.Println(s)
-	for _, merge := range [][]string{{"n", "ne", "nw"}, {"s", "se", "sw"}} {
+	mergePairs := [][]string{
+		{"n", "ne", "nw"},
+		{"s", "se", "sw"},
+		{"ne", "n", "se"},
+		{"nw", "n", "sw"},
+		{"se", "s", "ne"},
+		{"sw", "s", "nw"},
+	}
+	for _, merge := range mergePairs {
 		min := intmath.Min(s[merge[1]], s[merge[2]])
 		s[merge[0]] += min
 		s[merge[1]] -= min
 		s[merge[2]] -= min
 	}
-	return s["n"] + s["nw"] + s["ne"]
+	var sum int
+	for _, v := range s {
+		sum += v
+	}
+	return sum
 }
 
 func part1() {
@@ -40,6 +50,15 @@ func part1() {
 }
 
 func part2() {
+	var max int
+	in := input()
+	for i := 1522; i < len(in); i++ {
+		s := countSteps(in[0:i])
+		if s > max {
+			max = s
+		}
+	}
+	fmt.Println("The maximum distance was:", max)
 }
 
 func main() {
