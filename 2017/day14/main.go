8f74fbb00a7526c910724620749bce4c38dcd1ad
James Bogosian <bogosj@gmail.com>
2020-01-15 18:03:50 -0500

    Day 14 done.

--- 2017/day14/main.go
+++ 2017/day14/main.go
@@ -28,6 +28,9 @@ func makeMap(hashes []string) map[intmath.Point]int {
 		for _, c := range hash {
 			if i, err := strconv.ParseInt(string(c), 16, 64); err == nil {
 				bin := strconv.FormatInt(i, 2)
+				for len(bin) < 4 {
+					bin = "0" + bin
+				}
 				for _, c := range bin {
 					ret[intmath.Point{X: x, Y: y}] = int(c - '0')
 					x++
@@ -40,6 +43,36 @@ func makeMap(hashes []string) map[intmath.Point]int {
 	return ret
 }
 
+func explore(p intmath.Point, gid int, m, visited map[intmath.Point]int) int {
+	if _, ok := visited[p]; ok {
+		return gid
+	}
+	if m[p] == 0 {
+		visited[p] = 0
+		return gid
+	}
+	visited[p] = gid
+	for _, n := range p.Neighbors() {
+		if _, ok := m[n]; ok {
+			explore(n, gid, m, visited)
+		}
+	}
+	return gid + 1
+}
+
+func buildGroupMap(m map[intmath.Point]int) map[intmath.Point]int {
+	ret := map[intmath.Point]int{}
+	gid := 1
+
+	for y := 0; y < 128; y++ {
+		for x := 0; x < 128; x++ {
+			gid = explore(intmath.Point{X: x, Y: y}, gid, m, ret)
+		}
+	}
+
+	return ret
+}
+
 func part1() {
 	var sum int
 	for _, v := range makeMap(allHashes()) {
@@ -49,6 +82,14 @@ func part1() {
 }
 
 func part2() {
+	m := buildGroupMap(makeMap(allHashes()))
+	var max int
+	for _, v := range m {
+		if v > max {
+			max = v
+		}
+	}
+	fmt.Printf("There are %d groups in the map.\n", max)
 }
 
 func main() {
