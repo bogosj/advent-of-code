a8febcfca2a279849287a4fd9797d30cf365e14e
James Bogosian <bogosj@gmail.com>
2020-01-16 13:48:14 -0500

    Day 18 done

--- 2017/day18/main.go
+++ 2017/day18/main.go
@@ -56,12 +56,68 @@ func (c *computer) runToFirstRCV() int {
 	}
 }
 
+func (c *computer) run(in <-chan int) <-chan int {
+	out := make(chan int, 5000)
+	go func() {
+		for {
+			inst := c.inst[c.pc]
+			c.pc++
+			switch inst[0] {
+			case "snd":
+				out <- c.reg[inst[1]]
+			case "set":
+				c.reg[inst[1]] = c.readVal(inst[2])
+			case "add":
+				c.reg[inst[1]] += c.readVal(inst[2])
+			case "mul":
+				c.reg[inst[1]] *= c.readVal(inst[2])
+			case "mod":
+				c.reg[inst[1]] %= c.readVal(inst[2])
+			case "rcv":
+				c.reg[inst[1]] = <-in
+			case "jgz":
+				if c.readVal(inst[1]) > 0 {
+					c.pc += c.readVal(inst[2]) - 1
+				}
+			}
+		}
+	}()
+	return out
+}
+
 func part1() {
 	c := newComp()
 	fmt.Println("The first fcv is", c.runToFirstRCV())
 }
 
 func part2() {
+	c1 := newComp()
+	c2 := newComp()
+	c1.reg["p"] = 0
+	c2.reg["p"] = 1
+	in1 := make(chan int)
+	in2 := make(chan int)
+	o1 := c1.run(in1)
+	o2 := c2.run(in2)
+	var c, sleep int
+	for {
+		select {
+		case m := <-o1:
+			sleep = 0
+			in2 <- m
+		case m := <-o2:
+			sleep = 0
+			c++
+			in1 <- m
+		default:
+			time.Sleep(time.Second)
+			sleep++
+		}
+		if sleep >= 5 {
+			fmt.Printf("Computers have not responded for 5s, C2 sent %d messages\n", c)
+			return
+		}
+	}
 }
 
 func main() {
