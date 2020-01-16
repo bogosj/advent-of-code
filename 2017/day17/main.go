e6ea6e1a86db38ba0437d73d3e8c6481c08ed2a3
James Bogosian <bogosj@gmail.com>
2020-01-16 11:28:39 -0500

    Day 17 part 1 done

--- 2017/day17/main.go
+++ 2017/day17/main.go
@@ -5,7 +5,73 @@ import (
 	"time"
 )
 
+type node struct {
+	val  int
+	next *node
+}
+
+type spinLock struct {
+	head, curr *node
+	step       int
+}
+
+func newSpinLock() *spinLock {
+	n := node{val: 0}
+	n.next = &n
+	return &spinLock{
+		head: &n,
+		curr: &n,
+		step: 301,
+	}
+}
+
+func (s *spinLock) String() (ret string) {
+	var i int
+	n := s.head
+	for {
+		v := fmt.Sprintf("%d", n.val)
+		if n == s.curr {
+			v = "(" + v + ")"
+		}
+		ret += v
+		n = n.next
+		if n == s.head {
+			return
+		}
+		i++
+		ret += " "
+	}
+}
+
+func (s *spinLock) insert(i int) {
+	ip := s.curr
+	for i := 0; i < s.step; i++ {
+		ip = ip.next
+	}
+	n := &node{
+		val:  i,
+		next: ip.next,
+	}
+	s.curr = n
+	ip.next = n
+}
+
+func (s *spinLock) valueAfter(i int) int {
+	n := s.head
+	for {
+		if n.val == i {
+			return n.next.val
+		}
+		n = n.next
+	}
+}
+
 func part1() {
+	s := newSpinLock()
+	for i := 1; i <= 2017; i++ {
+		s.insert(i)
+	}
+	fmt.Printf("The value after %d is %d\n", 2017, s.valueAfter(2017))
 }
 
 func part2() {
