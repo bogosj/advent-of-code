94f1884f826408e7587059f2e69ee7a323906af2
James Bogosian <bogosj@gmail.com>
2020-01-17 19:57:17 -0500

    Day 23 part 1 done

--- 2017/day23/main.go
+++ 2017/day23/main.go
@@ -2,10 +2,61 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 	"time"
+
+	"github.com/bogosj/advent-of-code/fileinput"
 )
 
+type computer struct {
+	inst     [][]string
+	reg      map[string]int
+	pc       int
+	mulCount int
+}
+
+func newComp() *computer {
+	c := computer{reg: map[string]int{}}
+	for _, line := range fileinput.ReadLines("input.txt") {
+		c.inst = append(c.inst, strings.Fields(line))
+	}
+	return &c
+}
+
+func (c *computer) readVal(s string) int {
+	if v, err := strconv.Atoi(s); err == nil {
+		return v
+	}
+	return c.reg[s]
+}
+
+func (c *computer) run() {
+	l := len(c.inst)
+	for c.pc < l {
+		inst := c.inst[c.pc]
+		c.pc++
+		switch inst[0] {
+		case "set":
+			c.reg[inst[1]] = c.readVal(inst[2])
+		case "sub":
+			c.reg[inst[1]] -= c.readVal(inst[2])
+		case "mul":
+			c.mulCount++
+			c.reg[inst[1]] *= c.readVal(inst[2])
+		case "jnz":
+			if c.readVal(inst[1]) != 0 {
+				c.pc--
+				c.pc += c.readVal(inst[2])
+			}
+		}
+	}
+}
+
 func part1() {
+	c := newComp()
+	c.run()
+	fmt.Printf("The computer calls mul %d times\n", c.mulCount)
 }
 
 func part2() {
