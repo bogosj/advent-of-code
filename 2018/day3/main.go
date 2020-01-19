47b678f6117175b301fb9fe0006d388024a9c094
James Bogosian <bogosj@gmail.com>
2020-01-18 22:32:31 -0500

    Day 3 part 1 done

--- 2018/day3/main.go
+++ 2018/day3/main.go
@@ -2,10 +2,55 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"time"
+
+	"github.com/bogosj/advent-of-code/fileinput"
+	"github.com/bogosj/advent-of-code/intmath"
 )
 
+type point = intmath.Point
+
+func input() (ret [][]string) {
+	for _, line := range fileinput.ReadLines("input.txt") {
+		ret = append(ret, strings.Fields(line))
+	}
+	return
+}
+
+func getPoint(s string) point {
+	f := strings.Split(s, ",")
+	f[1] = strings.ReplaceAll(f[1], ":", "")
+	return point{X: intmath.Atoi(f[0]), Y: intmath.Atoi(f[1])}
+}
+
+func getDist(s string) (x, y int) {
+	f := strings.Split(s, "x")
+	return intmath.Atoi(f[0]), intmath.Atoi(f[1])
+}
+
+func makeOverlapMap() map[point]int {
+	ret := map[point]int{}
+	for _, claim := range input() {
+		p := getPoint(claim[2])
+		x1, y1 := getDist(claim[3])
+		for x := p.X; x < p.X+x1; x++ {
+			for y := p.Y; y < p.Y+y1; y++ {
+				ret[point{X: x, Y: y}]++
+			}
+		}
+	}
+	return ret
+}
+
 func part1() {
+	var count int
+	for _, v := range makeOverlapMap() {
+		if v > 1 {
+			count++
+		}
+	}
+	fmt.Println("The number of spots that are multi-claimed is:", count)
 }
 
 func part2() {
