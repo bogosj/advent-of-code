21f44830bb4196ed34a9992e9efa58fc76ccf2fb
James Bogosian <bogosj@gmail.com>
2020-01-18 22:00:45 -0500

    Day 2 complete

--- 2018/day2/main.go
+++ 2018/day2/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"time"
 
@@ -27,6 +28,37 @@ func checksum(s string) (two, three int) {
 	return
 }
 
+func cmp(s1, s2 string) (string, error) {
+	if s1 == s2 {
+		return "", errors.New("same string")
+	}
+
+	var shared string
+	var foundDiff bool
+	for i := 0; i < len(s1); i++ {
+		if s1[i] == s2[i] {
+			shared += string(s1[i])
+		} else {
+			if foundDiff == true {
+				return "", errors.New("too many diffs")
+			}
+			foundDiff = true
+		}
+	}
+	return shared, nil
+}
+
+func boxPairs(ss []string) string {
+	for _, s1 := range ss {
+		for _, s2 := range ss {
+			if v, err := cmp(s1, s2); err == nil {
+				return v
+			}
+		}
+	}
+	return ""
+}
+
 func part1() {
 	var twos, threes int
 	for _, s := range input() {
@@ -38,6 +70,7 @@ func part1() {
 }
 
 func part2() {
+	fmt.Println("The shared characters between box pairs is:", boxPairs(input()))
 }
 
 func main() {
