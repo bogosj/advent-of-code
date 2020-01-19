daac3665902e141ce07472d663d20344546aa4bf
James Bogosian <bogosj@gmail.com>
2020-01-19 11:02:47 -0500

    Day 5 part 1 done

--- 2018/day5/main.go
+++ 2018/day5/main.go
@@ -3,9 +3,39 @@ package main
 import (
 	"fmt"
 	"time"
+
+	"github.com/bogosj/advent-of-code/fileinput"
+	"github.com/bogosj/advent-of-code/intmath"
 )
 
+func input() []rune {
+	lines := fileinput.ReadLines("input.txt")
+	return []rune(lines[0])
+}
+
+func react(r []rune) (ret []rune) {
+	for i := 0; i < len(r); i++ {
+		if i == len(r)-1 {
+			ret = append(ret, r[i])
+			continue
+		}
+		if intmath.Abs(int(r[i])-int(r[i+1])) == 32 {
+			i++
+			continue
+		}
+		ret = append(ret, r[i])
+	}
+	return
+}
+
 func part1() {
+	in := input()
+	out := react(in)
+	for len(in) != len(out) {
+		in = out
+		out = react(in)
+	}
+	fmt.Println("The length of a fully processed polymer is:", len(out))
 }
 
 func part2() {
