b154c9ac22849c6c78f8ae2d17d2957df4e3fb9c
James Bogosian <bogosj@gmail.com>
2020-01-18 21:50:25 -0500

    Day 2 part 1 done

--- 2018/day2/main.go
+++ 2018/day2/main.go
@@ -3,9 +3,38 @@ package main
 import (
 	"fmt"
 	"time"
+
+	"github.com/bogosj/advent-of-code/fileinput"
 )
 
+func input() []string {
+	return fileinput.ReadLines("input.txt")
+}
+
+func checksum(s string) (two, three int) {
+	count := map[rune]int{}
+	for _, c := range s {
+		count[c]++
+	}
+	for _, v := range count {
+		switch v {
+		case 2:
+			two = 1
+		case 3:
+			three = 1
+		}
+	}
+	return
+}
+
 func part1() {
+	var twos, threes int
+	for _, s := range input() {
+		two, three := checksum(s)
+		twos += two
+		threes += three
+	}
+	fmt.Println("The checksum is:", twos*threes)
 }
 
 func part2() {
