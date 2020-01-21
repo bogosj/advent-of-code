c2fe71aa38fd99546273a6a07f488e93fbd14726
James Bogosian <bogosj@gmail.com>
2020-01-21 11:41:47 -0500

    Day 11 done

--- 2018/day11/main.go
+++ 2018/day11/main.go
@@ -38,22 +38,25 @@ func makeGrid() map[point]int {
 	return ret
 }
 
-func totalPower(x, y int, m map[point]int) (ret int) {
-	center := point{X: x + 1, Y: y + 1}
-	ret += m[center]
-	for _, n := range center.AllNeighbors() {
-		ret += m[n]
+func totalPower(cx, cy, size int, m map[point]int) (ret int) {
+	for x := cx; x < cx+size; x++ {
+		for y := cy; y < cy+size; y++ {
+			ret += m[point{X: x, Y: y}]
+		}
 	}
 	return
 }
 
-func strongestSquare(m map[point]int) (ret point) {
-	maxPower := -999999
-	for x := 1; x <= 298; x++ {
-		for y := 1; y <= 298; y++ {
-			power := totalPower(x, y, m)
-			if power > maxPower {
-				maxPower = power
+func strongestSquare(m map[point]int, size int) (ret point, retPower int) {
+	retPower = -999999
+	for x := 1; x <= 300; x++ {
+		if x+size > 300 {
+			continue
+		}
+		for y := 1; y <= 300; y++ {
+			power := totalPower(x, y, size, m)
+			if power > retPower {
+				retPower = power
 				ret = point{X: x, Y: y}
 			}
 		}
@@ -62,11 +65,16 @@ func strongestSquare(m map[point]int) (ret point) {
 }
 
 func part1() {
-	p := strongestSquare(makeGrid())
+	p, _ := strongestSquare(makeGrid(), 3)
 	fmt.Println("The top left cell of the most powerful square is:", p)
 }
 
 func part2() {
+	g := makeGrid()
+	for size := 1; size < 300; size++ {
+		p, power := strongestSquare(g, size)
+		fmt.Printf("The top left cell of the most powerful square is (size %d): %v, power:%v\n", size, p, power)
+	}
 }
 
 func main() {
