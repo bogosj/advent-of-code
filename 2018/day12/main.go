ae1097c2182b15d70dc5318644d012e95f508615
James Bogosian <bogosj@gmail.com>
2020-01-21 12:10:13 -0500

    Day 12 part 1 complete

--- 2018/day12/main.go
+++ 2018/day12/main.go
@@ -2,10 +2,87 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"time"
+
+	"github.com/bogosj/advent-of-code/fileinput"
 )
 
+const (
+	start = "####..##.##..##..#..###..#....#.######..###########.#...#.##..####.###.#.###.###..#.####..#.#..##..#"
+)
+
+func input() map[string]rune {
+	ret := map[string]rune{}
+	for _, line := range fileinput.ReadLines("input.txt") {
+		f := strings.Fields(line)
+		ret[f[0]] = rune(f[2][0])
+	}
+	return ret
+}
+
+func startState() map[int]rune {
+	s := map[int]rune{}
+	for i, v := range start {
+		s[i] = v
+	}
+	return s
+}
+
+func min(s map[int]rune) (ret int) {
+	ret = 10
+	for i := range s {
+		if i < ret {
+			ret = i
+		}
+	}
+	return
+}
+
+func max(s map[int]rune) (ret int) {
+	ret = 0
+	for i := range s {
+		if i > ret {
+			ret = i
+		}
+	}
+	return
+}
+
+func nextState(i int, s map[int]rune, t map[string]rune) rune {
+	var rs []rune
+	for j := i - 2; j <= i+2; j++ {
+		if r, ok := s[j]; ok {
+			rs = append(rs, r)
+		} else {
+			rs = append(rs, '.')
+		}
+	}
+	return t[string(rs)]
+}
+
+func evolve(s map[int]rune, t map[string]rune) map[int]rune {
+	ns := map[int]rune{}
+	start, end := min(s)-2, max(s)+2
+	for i := start; i <= end; i++ {
+		ns[i] = nextState(i, s, t)
+	}
+	return ns
+}
+
 func part1() {
+	transitions := input()
+	state := startState()
+	for i := 0; i < 20; i++ {
+		state = evolve(state, transitions)
+	}
+	var sum int
+	for i, v := range state {
+		if v == '#' {
+			sum += i
+		}
+	}
+	fmt.Println("The sum of all pots holding a plant after 20 generations is:", sum)
 }
 
 func part2() {
