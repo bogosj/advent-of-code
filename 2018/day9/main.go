550b75c3a0f37e43364684213e2ff3bffa85fbdc
James Bogosian <bogosj@gmail.com>
2020-01-20 22:23:14 -0500

    Day 9 part 1 done

--- 2018/day9/main.go
+++ 2018/day9/main.go
@@ -1,11 +1,65 @@
 package main
 
 import (
+	"container/ring"
 	"fmt"
 	"time"
 )
 
+type game struct {
+	scores      map[int]int
+	players     int
+	table, head *ring.Ring
+	step        int
+}
+
+func newGame(players int) *game {
+	g := game{}
+	g.scores = map[int]int{}
+	g.table = ring.New(1)
+	g.head = g.table
+	g.table.Value = 0
+	g.players = players
+	g.step = 1
+	return &g
+}
+
+func (g *game) winner() (retID, retScore int) {
+	for k, v := range g.scores {
+		if v > retScore {
+			retScore = v
+			retID = k
+		}
+	}
+	return
+}
+
+func (g *game) play(steps int) {
+	for g.step <= steps {
+		if g.step%23 == 0 {
+			pid := g.step % g.players
+			g.scores[pid] += g.step
+			for i := 0; i <= 7; i++ {
+				g.table = g.table.Prev()
+			}
+			marble := g.table.Unlink(1)
+			g.scores[pid] += marble.Value.(int)
+			g.table = g.table.Next()
+		} else {
+			marble := ring.New(1)
+			marble.Value = g.step
+			g.table = g.table.Next()
+			g.table = g.table.Link(marble)
+			g.table = g.table.Prev()
+		}
+		g.step++
+	}
+}
+
 func part1() {
+	g := newGame(419)
+	g.play(71052)
+	fmt.Println(g.winner())
 }
 
 func part2() {
