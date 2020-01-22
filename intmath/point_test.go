9d666e7e1e9a85671072aa9940b5420364ce044e
James Bogosian <bogosj@gmail.com>
2020-01-22 10:37:49 -0500

    Update point.Neighbor functions. Ensure points are returned in reading order.

--- intmath/point_test.go
+++ intmath/point_test.go
@@ -18,12 +18,12 @@ func TestPoint_Neighbors(t *testing.T) {
 		{
 			name:    "origin",
 			fields:  fields{X: 0, Y: 0},
-			wantRet: []Point{{X: -1, Y: 0}, {X: 1, Y: 0}, {X: 0, Y: -1}, {X: 0, Y: 1}},
+			wantRet: []Point{{0, -1}, {-1, 0}, {1, 0}, {0, 1}},
 		},
 		{
 			name:    "elsewhere",
 			fields:  fields{X: 1, Y: 1},
-			wantRet: []Point{{X: 0, Y: 1}, {X: 2, Y: 1}, {X: 1, Y: 0}, {X: 1, Y: 2}},
+			wantRet: []Point{{1, 0}, {0, 1}, {2, 1}, {1, 2}},
 		},
 	}
 	for _, tt := range tests {
@@ -86,6 +86,35 @@ func TestPoint_ManhattanDistanceTo(t *testing.T) {
 			}
 			if got := p.ManhattanDistanceTo(tt.args.op); got != tt.want {
 				t.Errorf("Point.ManhattanDistanceTo() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestPoint_AllNeighbors(t *testing.T) {
+	type fields struct {
+		X int
+		Y int
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		wantRet []Point
+	}{
+		{
+			name:    "simple",
+			fields:  fields{X: 1, Y: 1},
+			wantRet: []Point{{0, 0}, {1, 0}, {2, 0}, {0, 1}, {2, 1}, {0, 2}, {1, 2}, {2, 2}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			p := Point{
+				X: tt.fields.X,
+				Y: tt.fields.Y,
+			}
+			if gotRet := p.AllNeighbors(); !reflect.DeepEqual(gotRet, tt.wantRet) {
+				t.Errorf("Point.AllNeighbors() = %v, want %v", gotRet, tt.wantRet)
 			}
 		})
 	}
