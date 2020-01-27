5848f9417baa05e4b0d7e2c3587bb58984b10024
James Bogosian <bogosj@gmail.com>
2020-01-27 10:27:41 -0500

    Day 18 part 1 done.

--- 2018/day18/main.go
+++ 2018/day18/main.go
@@ -3,9 +3,85 @@ package main
 import (
 	"fmt"
 	"time"
+
+	"github.com/bogosj/advent-of-code/fileinput"
+	"github.com/bogosj/advent-of-code/intmath"
 )
 
+const (
+	open   = '.'
+	trees  = '|'
+	lumber = '#'
+)
+
+type point = intmath.Point
+
+func input() map[point]rune {
+	ret := map[point]rune{}
+	for y, line := range fileinput.ReadLines("input.txt") {
+		for x, c := range line {
+			ret[point{X: x, Y: y}] = c
+		}
+	}
+	return ret
+}
+
+func neighbors(p point, m map[point]rune) (cTrees, cLumber int) {
+	for _, v := range p.AllNeighbors() {
+		switch m[v] {
+		case trees:
+			cTrees++
+		case lumber:
+			cLumber++
+		}
+	}
+	return
+}
+
+func advance(m map[point]rune) map[point]rune {
+	ret := map[point]rune{}
+	for p, v := range m {
+		cTrees, cLumber := neighbors(p, m)
+		switch v {
+		case open:
+			ret[p] = open
+			if cTrees >= 3 {
+				ret[p] = trees
+			}
+		case trees:
+			ret[p] = trees
+			if cLumber >= 3 {
+				ret[p] = lumber
+			}
+		case lumber:
+			ret[p] = open
+			if cLumber > 0 && cTrees > 0 {
+				ret[p] = lumber
+			}
+		}
+	}
+	return ret
+}
+
+func resourceValue(m map[point]rune) int {
+	var cTrees, cLumber int
+	for _, v := range m {
+		switch v {
+		case trees:
+			cTrees++
+		case lumber:
+			cLumber++
+		}
+	}
+	return cTrees * cLumber
+}
+
 func part1() {
+	state := input()
+	for i := 0; i < 10; i++ {
+		state = advance(state)
+	}
+	fmt.Printf("The resource value after 10 minutes is: %d\n", resourceValue(state))
 }
 
 func part2() {
