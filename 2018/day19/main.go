94c5b15e7bb12c930a3cce85b537e298ad03652c
James Bogosian <bogosj@gmail.com>
2020-01-27 12:35:01 -0500

    Day 19 done.

--- 2018/day19/main.go
+++ 2018/day19/main.go
@@ -33,6 +33,35 @@ func (c *computer) setIPReg(s string) {
 	c.ipReg = intmath.Atoi(f[1])
 }
 
+func (c *computer) runInst(f []string, v []int) {
+	switch f[0] {
+	case "addi":
+		c.reg[v[2]] = c.reg[v[0]] + v[1]
+	case "addr":
+		c.reg[v[2]] = c.reg[v[0]] + c.reg[v[1]]
+	case "eqrr":
+		if c.reg[v[0]] == c.reg[v[1]] {
+			c.reg[v[2]] = 1
+		} else {
+			c.reg[v[2]] = 0
+		}
+	case "gtrr":
+		if c.reg[v[0]] > c.reg[v[1]] {
+			c.reg[v[2]] = 1
+		} else {
+			c.reg[v[2]] = 0
+		}
+	case "muli":
+		c.reg[v[2]] = c.reg[v[0]] * v[1]
+	case "mulr":
+		c.reg[v[2]] = c.reg[v[0]] * c.reg[v[1]]
+	case "seti":
+		c.reg[v[2]] = v[0]
+	case "setr":
+		c.reg[v[2]] = c.reg[v[0]]
+	}
+}
+
 func (c *computer) run() {
 	lines := fileinput.ReadLines("input.txt")
 	c.setIPReg(lines[0])
@@ -40,46 +69,42 @@ func (c *computer) run() {
 	for c.ip < len(inst) {
 		f := strings.Fields(inst[c.ip])
 		v := vals(f[1:])
-
 		c.reg[c.ipReg] = c.ip
-		switch f[0] {
-		case "addi":
-			c.reg[v[2]] = c.reg[v[0]] + v[1]
-		case "addr":
-			c.reg[v[2]] = c.reg[v[0]] + c.reg[v[1]]
-		case "eqrr":
-			if c.reg[v[0]] == c.reg[v[1]] {
-				c.reg[v[2]] = 1
-			} else {
-				c.reg[v[2]] = 0
-			}
-		case "gtrr":
-			if c.reg[v[0]] > c.reg[v[1]] {
-				c.reg[v[2]] = 1
-			} else {
-				c.reg[v[2]] = 0
-			}
-		case "muli":
-			c.reg[v[2]] = c.reg[v[0]] * v[1]
-		case "mulr":
-			c.reg[v[2]] = c.reg[v[0]] * c.reg[v[1]]
-		case "seti":
-			c.reg[v[2]] = v[0]
-		case "setr":
-			c.reg[v[2]] = c.reg[v[0]]
-		}
+		c.runInst(f, v)
 		c.ip = c.reg[c.ipReg]
 		c.ip++
 	}
 }
 
+func (c *computer) runFast() {
+	lines := fileinput.ReadLines("input.txt")
+	c.setIPReg(lines[0])
+	inst := lines[1:]
+	for c.ip < len(inst) {
+		f := strings.Fields(inst[c.ip])
+		v := vals(f[1:])
+		c.reg[c.ipReg] = c.ip
+		c.runInst(f, v)
+		c.ip = c.reg[c.ipReg]
+		c.ip++
+		if c.reg[0] == 1 && c.reg[4] > 0 {
+			c.reg[0] = intmath.Sum(intmath.Factors(c.reg[5])...)
+			return
+		}
+	}
+}
+
 func part1() {
 	c := newComp()
-	c.run()
+	c.runFast()
 	fmt.Printf("The value in register 0 is: %d\n", c.reg[0])
 }
 
 func part2() {
+	c := newComp()
+	c.reg[0] = 1
+	c.runFast()
+	fmt.Printf("The value in register 0 is: %d\n", c.reg[0])
 }
 
 func main() {
