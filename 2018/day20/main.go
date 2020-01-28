37b646bad23e85a554f931a260b0bb16e0902ee6
James Bogosian <bogosj@gmail.com>
2020-01-28 11:20:42 -0500

    Day 20 part 1 done

--- 2018/day20/main.go
+++ 2018/day20/main.go
@@ -2,10 +2,72 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"time"
+
+	"github.com/bogosj/advent-of-code/fileinput"
+	"github.com/bogosj/advent-of-code/intmath"
+	"github.com/gammazero/deque"
 )
 
+type point = intmath.Point
+
+func input() string {
+	s := fileinput.ReadLines("input.txt")[0]
+	s = strings.ReplaceAll(s, "^", "")
+	return strings.ReplaceAll(s, "$", "")
+}
+
+func buildMap(s string) map[point]string {
+	ret := map[point]string{}
+	stack := deque.Deque{}
+	curr := point{}
+	for _, c := range s {
+		switch c {
+		case '^', '$':
+		case '(':
+			stack.PushBack(curr)
+		case '|':
+			curr = stack.Back().(point)
+		case ')':
+			curr = stack.PopBack().(point)
+		default:
+			path := ret[curr]
+			curr = curr.Neighbor(c)
+			newPath := path + string(c)
+			if currPath, ok := ret[curr]; ok {
+				if len(currPath) > len(newPath) {
+					ret[curr] = newPath
+				}
+			} else {
+				ret[curr] = newPath
+			}
+		}
+	}
+	return ret
+}
+
+func maxPath(s string) (max int) {
+	m := buildMap(s)
+	for _, v := range m {
+		if len(v) > max {
+			max = len(v)
+		}
+	}
+	return
+}
+
+func testCases() {
+	s := "^ENNWSWW(NEWS|)SSSEEN(WNSE|)EE(SWEN|)NNN$"
+	fmt.Printf("Should be 18: %d\n", maxPath(s))
+	s = "^ESSWWN(E|NNENN(EESS(WNSE|)SSS|WWWSSSSE(SW|NNNE)))$"
+	fmt.Printf("Should be 23: %d\n", maxPath(s))
+	s = "^WSSEESWWWNW(S|NENNEEEENN(ESSSSW(NWSW|SSEN)|WSWWN(E|WWS(E|SS))))$"
+	fmt.Printf("Should be 31: %d\n", maxPath(s))
+}
+
 func part1() {
+	fmt.Println("The max path in the facility is:", maxPath(input()))
 }
 
 func part2() {
