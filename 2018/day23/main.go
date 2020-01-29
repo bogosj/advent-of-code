d6cea7b54a854e0daf2fff5018e49a2c77a207f7
James Bogosian <bogosj@gmail.com>
2020-01-29 11:07:17 -0500

    Day 23 complete

--- 2018/day23/main.go
+++ 2018/day23/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"sort"
 	"strings"
 	"time"
@@ -23,6 +24,10 @@ func (n *nanobot) canSee(on *nanobot) bool {
 	return n.r >= dist
 }
 
+func (n *nanobot) distToOrigin() int {
+	return intmath.Abs(n.x) + intmath.Abs(n.y) + intmath.Abs(n.z)
+}
+
 func input() (ret []*nanobot) {
 	for _, line := range fileinput.ReadLines("input.txt") {
 		b := &nanobot{}
@@ -41,6 +46,79 @@ func input() (ret []*nanobot) {
 	return
 }
 
+func bestPoint(bots []*nanobot) nanobot {
+	max, min, ranges := minMax(bots)
+	bestLocation := nanobot{}
+	grain := int(math.Pow(2, 23))
+	for {
+		for x := min.x; x < max.x; x += grain {
+			for y := min.y; y < max.y; y += grain {
+				for z := min.z; z < max.z; z += grain {
+					currPoint := nanobot{x: x, y: y, z: z}
+					for _, bot := range bots {
+						if bot.canSee(&currPoint) {
+							currPoint.r++
+						}
+					}
+					if currPoint.r == bestLocation.r {
+						if currPoint.distToOrigin() < bestLocation.distToOrigin() {
+							bestLocation = currPoint
+						}
+					}
+					if currPoint.r > bestLocation.r {
+						bestLocation = currPoint
+					}
+				}
+			}
+		}
+		fmt.Printf("grain:%v min:%v max:%v ranges:%v best:%v\n", grain, min, max, ranges, bestLocation)
+		if grain == 1 {
+			break
+		}
+		grain /= 2
+		ranges.x /= 2
+		ranges.y /= 2
+		ranges.z /= 2
+		min.x = bestLocation.x - ranges.x/2
+		min.y = bestLocation.y - ranges.y/2
+		min.z = bestLocation.z - ranges.z/2
+		max.x = bestLocation.x + ranges.x/2
+		max.y = bestLocation.y + ranges.y/2
+		max.z = bestLocation.z + ranges.z/2
+	}
+	fmt.Printf("%v\n", bestLocation)
+	fmt.Printf("Distance to origin for best point is: %d\n", bestLocation.distToOrigin())
+	return bestLocation
+}
+
+func minMax(bots []*nanobot) (nanobot, nanobot, nanobot) {
+	var min, max, ranges nanobot
+	for _, bot := range bots {
+		if bot.x < min.x {
+			min.x = bot.x
+		}
+		if bot.y < min.y {
+			min.y = bot.y
+		}
+		if bot.z < min.z {
+			min.z = bot.z
+		}
+		if bot.x > max.x {
+			max.x = bot.x
+		}
+		if bot.y > max.y {
+			max.y = bot.y
+		}
+		if bot.z > max.z {
+			max.z = bot.z
+		}
+	}
+	ranges.x = max.x - min.x
+	ranges.y = max.y - min.y
+	ranges.z = max.z - min.z
+	return max, min, ranges
+}
+
 func part1() {
 	bots := input()
 	var c int
@@ -53,6 +131,7 @@ func part1() {
 }
 
 func part2() {
+	bestPoint(input())
 }
 
 func main() {
