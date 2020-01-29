908c0d1c0d9536c03fecd7898fd6884c30e316f9
James Bogosian <bogosj@gmail.com>
2020-01-28 23:36:57 -0500

    Day 22 complete.

--- 2018/day22/main.go
+++ 2018/day22/main.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/bogosj/advent-of-code/intmath"
+	pq "github.com/jupp0r/go-priority-queue"
 )
 
 type point = intmath.Point
@@ -14,6 +15,12 @@ const (
 	rocky = iota
 	wet
 	narrow
+)
+
+const (
+	neither = iota
+	torch
+	gear
 )
 
 type cave struct {
@@ -41,7 +48,7 @@ func (c *cave) regionType(p point) regionType {
 func (c *cave) geoIndex(p point) (ret int) {
 	switch v, ok := c.geoIndexes[p]; {
 	case ok:
-		ret = v
+		return v
 	case p.ManhattanDistanceTo(point{}) == 0:
 	case p.ManhattanDistanceTo(c.target) == 0:
 	case p.Y == 0:
@@ -64,12 +71,79 @@ func (c *cave) riskLevel() (risk int) {
 	return
 }
 
+type state struct {
+	pos     point
+	minutes int
+	equip   int
+}
+
+func (s *state) String() string {
+	return fmt.Sprintf("%v: %v", s.pos, s.minutes)
+}
+
+func (s *state) isBad(c *cave) bool {
+	return s.equip == c.regionType(s.pos)
+}
+
+func (c *cave) nextStates(curr *state) (ret []*state) {
+	for _, n := range curr.pos.Neighbors() {
+		if n.X < 0 || n.Y < 0 {
+			continue
+		}
+		ret = append(ret, &state{pos: n, minutes: curr.minutes + 1, equip: curr.equip})
+		for i := 1; i <= 2; i++ {
+			ns := &state{pos: curr.pos, minutes: curr.minutes + 8, equip: (curr.equip + i) % 3}
+			if !ns.isBad(c) {
+				ns.pos = n
+				ret = append(ret, ns)
+			}
+		}
+	}
+	return
+}
+
+func (c *cave) navigate() *state {
+	states := pq.New()
+	states.Insert(&state{equip: torch}, 0)
+	seen := map[state]bool{}
+	for states.Len() > 0 {
+		i, err := states.Pop()
+		if err != nil {
+			panic(err)
+		}
+		curr := i.(*state)
+		if curr.isBad(c) {
+			continue
+		}
+		cs := state{pos: curr.pos, equip: curr.equip}
+		if seen[cs] == true {
+			continue
+		}
+		seen[cs] = true
+		if curr.pos.ManhattanDistanceTo(c.target) == 0 {
+			if curr.equip == torch {
+				return curr
+			}
+			curr.equip = torch
+			curr.minutes += 7
+			states.Insert(curr, float64(curr.minutes))
+			continue
+		}
+		for _, s := range c.nextStates(curr) {
+			states.Insert(s, float64(s.minutes))
+		}
+	}
+	return &state{}
+}
+
 func part1() {
 	c := newCave()
 	fmt.Println("The risk level of this quadrant is:", c.riskLevel())
 }
 
 func part2() {
+	c := newCave()
+	fmt.Println("The shortest path to the target in minutes is:", c.navigate().minutes)
 }
 
 func main() {
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.13
 
 require (
 	github.com/gammazero/deque v0.0.0-20190521012701-46e4ffb7a622
+	github.com/jupp0r/go-priority-queue v0.0.0-20160601094913-ab1073853bde
 	github.com/mxschmitt/golang-combinations v1.0.0
 	github.com/pkg/profile v1.4.0
 	gonum.org/v1/gonum v0.6.2
