b69404fd94913fae6fdd451c7b9c287efc4079c3
James Bogosian <bogosj@gmail.com>
2020-01-29 20:52:40 -0500

    Day 24 part 1 done.

--- 2018/day24/main.go
+++ 2018/day24/main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"fmt"
+	"sort"
 	"strings"
 	"time"
 
 	"github.com/bogosj/advent-of-code/fileinput"
 	"github.com/bogosj/advent-of-code/intmath"
 )
+
+// 21760 too lows
 
 const (
 	immune = iota
@@ -18,8 +21,58 @@ type group struct {
 	damage, hp        int
 	units, initiative int
 	team              int
-	attack            string
+	attackType        string
 	immune, weak      []string
+}
+
+func (g *group) String() string {
+	return fmt.Sprintf("i%d w/ %d units w/ %d HP", g.initiative, g.units, g.hp)
+}
+
+func (g *group) effectivePower() int {
+	return g.units * g.damage
+}
+
+func (g *group) damageCausedBy(attacker *group) (ret int) {
+	ret = attacker.effectivePower()
+	for _, t := range g.immune {
+		if attacker.attackType == t {
+			ret = 0
+		}
+	}
+	for _, t := range g.weak {
+		if attacker.attackType == t {
+			ret *= 2
+		}
+	}
+	return
+}
+
+func (g *group) attack(og *group) {
+	if og == nil {
+		return
+	}
+	unitsKilled := intmath.Min(og.damageCausedBy(g)/og.hp, og.units)
+	og.units -= unitsKilled
+}
+
+func (g *group) targets(groups []*group) (ret []*group) {
+	for _, og := range groups {
+		if og.team == g.team {
+			continue
+		}
+		ret = append(ret, og)
+	}
+	sort.Slice(ret, func(i, j int) bool {
+		if ret[i].damageCausedBy(g) == ret[j].damageCausedBy(g) {
+			if ret[i].effectivePower() == ret[j].effectivePower() {
+				return ret[i].initiative > ret[j].initiative
+			}
+			return ret[i].effectivePower() > ret[j].effectivePower()
+		}
+		return ret[i].damageCausedBy(g) > ret[j].damageCausedBy(g)
+	})
+	return ret
 }
 
 func input(s string) (ret []*group) {
@@ -69,7 +122,7 @@ func input(s string) (ret []*group) {
 			}
 		}
 		ng.damage = atoi(f[5])
-		ng.attack = f[6]
+		ng.attackType = f[6]
 		ng.initiative = atoi(f[10])
 		ng.team = team
 		ret = append(ret, &ng)
@@ -77,11 +130,84 @@ func input(s string) (ret []*group) {
 	return
 }
 
+func allOfOneTeam(groups []*group) bool {
+	firstTeam := groups[0].team
+	for i := 1; i < len(groups); i++ {
+		if groups[i].team != firstTeam {
+			return false
+		}
+	}
+	return true
+}
+
+func removeDeadGroups(groups []*group) (ret []*group) {
+	for _, g := range groups {
+		if g.units > 0 {
+			ret = append(ret, g)
+		}
+	}
+	return
+}
+
+func fightOneRound(groups []*group) []*group {
+	// sort by power and initiative
+	sort.Slice(groups, func(i, j int) bool {
+		if groups[i].effectivePower() == groups[j].effectivePower() {
+			return groups[i].initiative > groups[j].initiative
+		}
+		return groups[i].effectivePower() > groups[j].effectivePower()
+	})
+	// select targets
+	targets := map[*group]*group{}
+	targeted := map[*group]bool{}
+	for _, g := range groups {
+		for _, t := range g.targets(groups) {
+			if !targeted[t] {
+				if t.damageCausedBy(g) == 0 {
+					continue
+				}
+				targeted[t] = true
+				targets[g] = t
+				break
+			}
+		}
+	}
+	// sort by decreasing initiative
+	sort.Slice(groups, func(i, j int) bool {
+		return groups[i].initiative > groups[j].initiative
+	})
+	// perform attack
+	for _, g := range groups {
+		if g.units == 0 {
+			fmt.Printf("%v was dead, didn't attack\n", g)
+			continue
+		}
+		g.attack(targets[g])
+	}
+	return removeDeadGroups(groups)
+}
+
+func performCombat(groups []*group) (winners []*group) {
+	for {
+		if allOfOneTeam(groups) {
+			for _, g := range groups {
+				winners = append(winners, g)
+			}
+			return
+		}
+		groups = fightOneRound(groups)
+	}
+}
+
 func part1() {
 	groups := input("input.txt")
-	for _, group := range groups {
-		fmt.Println(group)
+	winners := performCombat(groups)
+	var total int
+	for _, g := range winners {
+		fmt.Println(g)
+		total += g.units
 	}
+	fmt.Printf("The winning army has %d units.\n", total)
 }
 
 func part2() {
