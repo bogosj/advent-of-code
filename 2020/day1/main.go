3a424935012589082902d0ddf97f5d3165c3a3a8
James Bogosian <bogosj@gmail.com>
2020-12-02 09:24:06 -0500

    Day 1 part 1 done

---
+++ 2020/day1/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"time"
+
+	"github.com/bogosj/advent-of-code/fileinput"
+)
+
+func part1() {
+	in := input()
+	for i, v1 := range in {
+		for j, v2 := range in {
+			if i != j {
+				if v1+v2 == 2020 {
+					fmt.Printf("Part 1 answer: %v\n", v1*v2)
+					return
+				}
+			}
+		}
+	}
+}
+
+func part2() {
+
+}
+
+func main() {
+	start := time.Now()
+	part1()
+	fmt.Println("Part 1 done in", time.Since(start))
+	start = time.Now()
+	part2()
+	fmt.Println("Part 2 done in", time.Since(start))
+}
+
+func input() []int {
+	var ret []int
+	lines := fileinput.ReadLines("input.txt")
+	for _, v := range lines {
+		iv, _ := strconv.Atoi(v)
+		ret = append(ret, iv)
+	}
+	return ret
+}
