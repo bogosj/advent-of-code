8dd121e88c7131fdd4d11b465cb81bc71dbf1172
James Bogosian <bogosj@gmail.com>
2020-12-04 10:10:44 -0500

    Day 3 part 2 done.

--- 2020/day3/main.go
+++ 2020/day3/main.go
@@ -16,19 +16,30 @@ func (f *forest) isTree(x, y int) bool {
 	return row[x%len(row)] == '#'
 }
 
-func part1(in forest) {
+func (f *forest) slopeCheck(right, down int) int {
 	c := 0
 	x := 0
-	for y := range in.area {
-		if in.isTree(x, y) {
+	for y := 0; y < len(f.area); y += down {
+		if f.isTree(x, y) {
 			c++
 		}
-		x += 3
+		x += right
 	}
+	return c
+}
+
+func part1(in forest) {
+	c := in.slopeCheck(3, 1)
 	fmt.Printf("Found %v trees along the path.\n", c)
 }
 
 func part2(in forest) {
+	a := (in.slopeCheck(1, 1) *
+		in.slopeCheck(3, 1) *
+		in.slopeCheck(5, 1) *
+		in.slopeCheck(7, 1) *
+		in.slopeCheck(1, 2))
+	fmt.Printf("Multipled slopes gets you %v.\n", a)
 }
 
 func main() {
