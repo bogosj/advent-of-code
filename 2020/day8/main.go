9c4d21bdb5a20c49b616d30821cf5815d1294f3f
James Bogosian <bogosj@gmail.com>
2020-12-08 14:09:29 -0500

    Day 8 part 1 done

--- 2020/day8/main.go
+++ 2020/day8/main.go
@@ -2,33 +2,65 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/bogosj/advent-of-code/fileinput"
+	"github.com/bogosj/advent-of-code/intmath"
 )
 
+type instruction struct {
+	code string
+	val  int
+}
+
+type console struct {
+	pc, acc  int
+	inst     []instruction
+	executed map[int]bool
+}
+
+func (c *console) executeUntilDupe() {
+	for !c.executed[c.pc] {
+		c.executed[c.pc] = true
+		i := c.inst[c.pc]
+		switch i.code {
+		case "nop":
+			c.pc++
+		case "acc":
+			c.acc += i.val
+			c.pc++
+		case "jmp":
+			c.pc += i.val
+		}
+	}
+}
+
+func newConsole(in []string) console {
+	c := console{}
+	for _, line := range in {
+		f := strings.Split(line, " ")
+		c.inst = append(c.inst, instruction{code: f[0], val: intmath.Atoi(f[1])})
+	}
+	c.executed = map[int]bool{}
+	return c
+}
+
 func part1(in []string) {
+	c := newConsole(in)
+	c.executeUntilDupe()
+	fmt.Printf("The val of acc before a dupe instruction is: %v\n", c.acc)
 }
 
 func part2(in []string) {
 }
 
 func main() {
-	in := input()
+	in := fileinput.ReadLines("input.txt")
 	start := time.Now()
 	part1(in)
 	fmt.Println("Part 1 done in", time.Since(start))
 	start = time.Now()
 	part2(in)
 	fmt.Println("Part 2 done in", time.Since(start))
-}
-
-func input() []string {
-	ret := []string{}
-
-	for _, line := range fileinput.ReadLines("input.txt") {
-		ret = append(ret, line)
-	}
-
-	return ret
 }
