09494cff71eea7499581b5bee6e5faa7a5aa1c72
James Bogosian <bogosj@gmail.com>
2020-12-09 10:16:54 -0500

    Day 9 done

--- 2020/day9/main.go
+++ 2020/day9/main.go
@@ -8,7 +8,7 @@ import (
 	"github.com/bogosj/advent-of-code/intmath"
 )
 
-func part1(in []int) {
+func part1(in []int) int {
 OUTER:
 	for i := 25; i < len(in); i++ {
 		for j := i - 25; j < i; j++ {
@@ -19,20 +19,34 @@ OUTER:
 			}
 		}
 		fmt.Printf("The first bad number is %v\n", in[i])
-		return
+		return in[i]
 	}
+	return -1
 }
 
-func part2(in []int) {
+func part2(target int, in []int) {
+OUTER:
+	for i := 0; i < len(in); i++ {
+		for j := i + 1; j < len(in); j++ {
+			sum := intmath.Sum(in[i:j]...)
+			if sum == target {
+				fmt.Printf("Weakness is %v\n", intmath.Min(in[i:j]...)+intmath.Max(in[i:j]...))
+				return
+			}
+			if sum > target {
+				continue OUTER
+			}
+		}
+	}
 }
 
 func main() {
 	in := input()
 	start := time.Now()
-	part1(in)
+	target := part1(in)
 	fmt.Println("Part 1 done in", time.Since(start))
 	start = time.Now()
-	part2(in)
+	part2(target, in)
 	fmt.Println("Part 2 done in", time.Since(start))
 }
 
