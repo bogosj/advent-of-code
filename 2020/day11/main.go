f598552bba9c228eab444b8ca4eceb57fa5f7322
James Bogosian <bogosj@gmail.com>
2020-12-11 09:33:04 -0500

    Day 11 done

--- 2020/day11/main.go
+++ 2020/day11/main.go
@@ -62,6 +62,53 @@ func (b *boat) next() (changed bool) {
 	return
 }
 
+func (b *boat) firstSeat(x, y int, p intmath.Point) *intmath.Point {
+	if x == 0 && y == 0 {
+		return nil
+	}
+	np := intmath.Point{Y: p.Y + y, X: p.X + x}
+	for {
+		if np.X < 0 || np.X >= len(b.rows[0]) || np.Y < 0 || np.Y >= len(b.rows) {
+			return nil
+		}
+		if b.rows[np.Y][np.X] == 'L' || b.rows[np.Y][np.X] == '#' {
+			return &np
+		}
+		np = intmath.Point{Y: np.Y + y, X: np.X + x}
+	}
+}
+
+func (b *boat) next2() (changed bool) {
+	newState := [][]rune{}
+	for y, row := range b.rows {
+		nr := []rune{}
+		for x, seat := range row {
+			p := intmath.Point{X: x, Y: y}
+			c := 0
+			for _, dY := range []int{-1, 0, 1} {
+				for _, dX := range []int{-1, 0, 1} {
+					ns := b.firstSeat(dX, dY, p)
+					if ns != nil && b.rows[ns.Y][ns.X] == '#' {
+						c++
+					}
+				}
+			}
+			if seat == 'L' && c == 0 {
+				nr = append(nr, '#')
+				changed = true
+			} else if seat == '#' && c >= 5 {
+				nr = append(nr, 'L')
+				changed = true
+			} else {
+				nr = append(nr, seat)
+			}
+		}
+		newState = append(newState, nr)
+	}
+	b.rows = newState
+	return
+}
+
 func part1(b boat) {
 	i := 0
 	for {
@@ -74,6 +121,14 @@ func part1(b boat) {
 }
 
 func part2(b boat) {
+	i := 0
+	for {
+		i++
+		if !b.next2() {
+			fmt.Printf("It took %v iterations to find the layout of %v seats.\n", i, b.occupiedSeats())
+			return
+		}
+	}
 }
 
 func main() {
