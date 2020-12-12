15eb20ee6788feb9a84494953350f22c335984e0
James Bogosian <bogosj@gmail.com>
2020-12-12 11:01:13 -0500

    Day 12 done

--- 2020/day12/main.go
+++ 2020/day12/main.go
@@ -16,9 +16,10 @@ const (
 )
 
 type boat struct {
-	inst []string
-	pos  intmath.Point
-	dir  int
+	inst     []string
+	pos      intmath.Point
+	dir      int
+	waypoint intmath.Point
 }
 
 func (b *boat) String() string {
@@ -43,7 +44,7 @@ func (b *boat) run() {
 		case 'R':
 			b.dir += n / 90
 		case 'F':
-			switch (b.dir+4000) % 4 {
+			switch (b.dir + 4000) % 4 {
 			case N:
 				b.pos.Y += n
 			case S:
@@ -54,7 +55,42 @@ func (b *boat) run() {
 				b.pos.X -= n
 			}
 		}
-		fmt.Printf("%v: %v\n", i, b)
+	}
+}
+
+func (b *boat) rotate(n int, d rune) {
+	if d == 'L' {
+		for i := 0; i < n; i++ {
+			b.waypoint.Y *= -1
+			b.waypoint = intmath.Point{X: b.waypoint.Y, Y: b.waypoint.X}
+		}
+	} else {
+		for i := 0; i < n; i++ {
+			b.waypoint.X *= -1
+			b.waypoint = intmath.Point{X: b.waypoint.Y, Y: b.waypoint.X}
+		}
+	}
+}
+
+func (b *boat) run2() {
+	for _, i := range b.inst {
+		r := i[0]
+		n := intmath.Atoi(i[1:])
+		switch r {
+		case 'N':
+			b.waypoint.Y += n
+		case 'S':
+			b.waypoint.Y -= n
+		case 'E':
+			b.waypoint.X += n
+		case 'W':
+			b.waypoint.X -= n
+		case 'L', 'R':
+			b.rotate(n/90, rune(r))
+		case 'F':
+			b.pos.Y += n * b.waypoint.Y
+			b.pos.X += n * b.waypoint.X
+		}
 	}
 }
 
@@ -64,6 +100,8 @@ func part1(b *boat) {
 }
 
 func part2(b *boat) {
+	b.run2()
+	fmt.Printf("The boat is %v away from start.\n", b.pos.ManhattanDistanceTo(intmath.Point{}))
 }
 
 func main() {
@@ -79,7 +117,8 @@ func main() {
 
 func input() *boat {
 	return &boat{
-		inst: fileinput.ReadLines("input.txt"),
-		dir:  E,
+		inst:     fileinput.ReadLines("input.txt"),
+		dir:      E,
+		waypoint: intmath.Point{X: 10, Y: 1},
 	}
 }
