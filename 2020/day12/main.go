c3ff28c77520b7bfbf15a9ba03d54e0bcf807c6a
James Bogosian <bogosj@gmail.com>
2020-12-12 10:40:31 -0500

    Day 12 part 1 done.

--- 2020/day12/main.go
+++ 2020/day12/main.go
@@ -5,12 +5,65 @@ import (
 	"time"
 
 	"github.com/bogosj/advent-of-code/fileinput"
+	"github.com/bogosj/advent-of-code/intmath"
 )
 
-func part1(in []string) {
+const (
+	E = iota
+	S
+	W
+	N
+)
+
+type boat struct {
+	inst []string
+	pos  intmath.Point
+	dir  int
 }
 
-func part2(in []string) {
+func (b *boat) String() string {
+	return fmt.Sprintf("pos: %v, dir: %v", b.pos, b.dir)
+}
+
+func (b *boat) run() {
+	for _, i := range b.inst {
+		r := i[0]
+		n := intmath.Atoi(i[1:])
+		switch r {
+		case 'N':
+			b.pos.Y += n
+		case 'S':
+			b.pos.Y -= n
+		case 'E':
+			b.pos.X += n
+		case 'W':
+			b.pos.X -= n
+		case 'L':
+			b.dir -= n / 90
+		case 'R':
+			b.dir += n / 90
+		case 'F':
+			switch (b.dir+4000) % 4 {
+			case N:
+				b.pos.Y += n
+			case S:
+				b.pos.Y -= n
+			case E:
+				b.pos.X += n
+			case W:
+				b.pos.X -= n
+			}
+		}
+		fmt.Printf("%v: %v\n", i, b)
+	}
+}
+
+func part1(b *boat) {
+	b.run()
+	fmt.Printf("The boat is %v away from start.\n", b.pos.ManhattanDistanceTo(intmath.Point{}))
+}
+
+func part2(b *boat) {
 }
 
 func main() {
@@ -18,17 +71,15 @@ func main() {
 	start := time.Now()
 	part1(in)
 	fmt.Println("Part 1 done in", time.Since(start))
+	in = input()
 	start = time.Now()
 	part2(in)
 	fmt.Println("Part 2 done in", time.Since(start))
 }
 
-func input() []string {
-	ret := []string{}
-
-	for _, line := range fileinput.ReadLines("input.txt") {
-		ret = append(ret, line)
+func input() *boat {
+	return &boat{
+		inst: fileinput.ReadLines("input.txt"),
+		dir:  E,
 	}
-
-	return ret
 }
