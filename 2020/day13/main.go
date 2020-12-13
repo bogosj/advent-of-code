b69c568c8bb4949ff235d46bac2a3a338e9f76c8
James Bogosian <bogosj@gmail.com>
2020-12-13 15:04:39 -0500

    Day 13 done

--- 2020/day13/main.go
+++ 2020/day13/main.go
@@ -37,7 +37,49 @@ func part1(in schedule) {
 	fmt.Printf("Waiting fur bus %v for %v minutes: %v\n", minB, minT, minT*minB)
 }
 
+type bus struct {
+	period, offset int
+}
+
+func (b *bus) inPosition(t int) bool {
+	return intmath.Mod(t+b.offset, b.period) == 0
+}
+
+func offsets(in []string) (ret []bus) {
+	for i, b := range in {
+		if b != "x" {
+			ret = append(ret, bus{period: intmath.Atoi(b), offset: i})
+		}
+	}
+	return
+}
+
+func departureTime(in []string) int {
+	o := offsets(in)
+	timeWarp := 1
+	t := 0
+	toFind := map[bus]bool{}
+	for _, b := range o {
+		toFind[b] = true
+	}
+	for {
+		for b := range toFind {
+			if b.inPosition(t) {
+				timeWarp *= b.period
+				delete(toFind, b)
+			}
+		}
+		if len(toFind) == 0 {
+			break
+		}
+		t += timeWarp
+	}
+	return t
+}
+
 func part2(in schedule) {
+	t := departureTime(in.buses)
+	fmt.Printf("The t where all buses are offset correctly is %v\n", t)
 }
 
 func main() {
