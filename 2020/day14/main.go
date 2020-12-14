d52f3049ee48840580b88cfd07dc78a645670b2e
James Bogosian <bogosj@gmail.com>
2020-12-14 12:14:33 -0500

    Day 14 part 1 done

--- 2020/day14/main.go
+++ 2020/day14/main.go
@@ -2,15 +2,59 @@ package main
 
 import (
 	"fmt"
+	"regexp"
+	"strings"
 	"time"
 
 	"github.com/bogosj/advent-of-code/fileinput"
+	"github.com/bogosj/advent-of-code/intmath"
 )
 
-func part1(in []string) {
+var (
+	memReg = regexp.MustCompile(`mem\[(\d+)\] = (\d+)`)
+)
+
+type computer struct {
+	inst []string
+	mem  map[int]int
+	mask string
 }
 
-func part2(in []string) {
+func (c *computer) setMem(line string) {
+	match := memReg.FindStringSubmatch(line)
+	addr := intmath.Atoi(match[1])
+	val := intmath.Atoi(match[2])
+	for i, bit := 0, 1<<35; i < len(c.mask); i, bit = i+1, bit>>1 {
+		switch c.mask[i] {
+		case '0':
+			val = val &^ bit
+		case '1':
+			val = val | bit
+		}
+	}
+	c.mem[addr] = val
+}
+
+func (c *computer) init() {
+	for _, line := range c.inst {
+		if strings.HasPrefix(line, "mask") {
+			c.mask = strings.TrimPrefix(line, "mask = ")
+		} else {
+			c.setMem(line)
+		}
+	}
+}
+
+func part1(in *computer) {
+	in.init()
+	a := 0
+	for _, v := range in.mem {
+		a += v
+	}
+	fmt.Printf("The sum of values is %v\n", a)
+}
+
+func part2(in *computer) {
 }
 
 func main() {
@@ -23,12 +67,9 @@ func main() {
 	fmt.Println("Part 2 done in", time.Since(start))
 }
 
-func input() []string {
-	ret := []string{}
-
-	for _, line := range fileinput.ReadLines("input.txt") {
-		ret = append(ret, line)
+func input() *computer {
+	return &computer{
+		inst: fileinput.ReadLines("input.txt"),
+		mem:  map[int]int{},
 	}
-
-	return ret
 }
