e75958a2340e6875654f047087dce8c2118e48ba
James Bogosian <bogosj@gmail.com>
2020-12-15 14:21:52 -0500

    Use a slice instead of a map.

--- 2020/day15/main.go
+++ 2020/day15/main.go
@@ -6,14 +6,13 @@ import (
 )
 
 func solve(count int) {
-	turns := map[int][]int{
-		6:  []int{5},
-		0:  []int{4},
-		11: []int{3},
-		10: []int{2},
-		1:  []int{1},
-		2:  []int{0},
-	}
+	turns := make([][]int, count)
+	turns[6] = []int{5}
+	turns[0] = []int{4}
+	turns[11] = []int{3}
+	turns[10] = []int{2}
+	turns[1] = []int{1}
+	turns[2] = []int{0}
 	last := 6
 	for i := 6; i < count; i++ {
 		next := 0
