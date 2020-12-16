1d299483fd45add77f8dcac2176837ea59553491
James Bogosian <bogosj@gmail.com>
2020-12-16 11:11:52 -0500

    Day 16 part 1 complete.

--- 2020/day16/main.go
+++ 2020/day16/main.go
@@ -2,34 +2,89 @@ package main
 
 import (
 	"fmt"
+	"regexp"
+	"strings"
 	"time"
+
+	"github.com/bogosj/advent-of-code/intmath"
 
 	"github.com/bogosj/advent-of-code/fileinput"
 )
 
-func part1(in []string) {
+var (
+	ruleRegex = regexp.MustCompile(`(.*): (.*) or (.*)`)
+)
+
+type rules struct {
+	r []string
 }
 
-func part2(in []string) {
+func (r *rules) isValid(i int) bool {
+	for _, r := range r.r {
+		m := ruleRegex.FindStringSubmatch(r)
+		for _, g := range m[2:] {
+			v := strings.Split(g, "-")
+			if i >= intmath.Atoi(v[0]) && i <= intmath.Atoi(v[1]) {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+type ticket struct {
+	val []int
+}
+
+func part1(r rules, mt *ticket, ot []ticket) {
+	invalid := []int{}
+	for _, t := range ot {
+		for _, i := range t.val {
+			if !r.isValid(i) {
+				invalid = append(invalid, i)
+			}
+		}
+	}
+	fmt.Printf("The sum of invalid fields is %v.\n", intmath.Sum(invalid...))
+}
+
+func part2(r rules, mt *ticket, ot []ticket) {
 }
 
 func main() {
-	in := input()
+	r, mt, ot := input()
 	start := time.Now()
-	part1(in)
+	part1(r, mt, ot)
 	fmt.Println("Part 1 done in", time.Since(start))
-	in = input()
+	r, mt, ot = input()
 	start = time.Now()
-	part2(in)
+	part2(r, mt, ot)
 	fmt.Println("Part 2 done in", time.Since(start))
 }
 
-func input() []string {
-	ret := []string{}
+func input() (rules, *ticket, []ticket) {
+	d := fileinput.ReadLines("input.txt")
+	r := rules{}
+	var mt *ticket
+	ot := []ticket{}
 
-	for _, line := range fileinput.ReadLines("input.txt") {
-		ret = append(ret, line)
+	for _, line := range d {
+		if ruleRegex.MatchString(line) {
+			r.r = append(r.r, line)
+		} else if strings.Contains(line, ",") {
+			if mt == nil {
+				mt = &ticket{}
+				for _, v := range strings.Split(line, ",") {
+					mt.val = append(mt.val, intmath.Atoi(v))
+				}
+			} else {
+				t := ticket{}
+				for _, v := range strings.Split(line, ",") {
+					t.val = append(t.val, intmath.Atoi(v))
+				}
+				ot = append(ot, t)
+			}
+		}
 	}
-
-	return ret
+	return r, mt, ot
 }
