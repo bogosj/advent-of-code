b71e2503a94319ba6e8542f4c5d8a36d2257be71
James Bogosian <bogosj@gmail.com>
2020-12-16 15:40:28 -0500

    day 16 part 2 done

--- 2020/day16/main.go
+++ 2020/day16/main.go
@@ -6,9 +6,8 @@ import (
 	"strings"
 	"time"
 
-	"github.com/bogosj/advent-of-code/intmath"
-
 	"github.com/bogosj/advent-of-code/fileinput"
+	"github.com/bogosj/advent-of-code/intmath"
 )
 
 var (
@@ -32,6 +31,17 @@ func (r *rules) isValid(i int) bool {
 	return false
 }
 
+func (r *rules) validForRule(ruleIDx, i int) bool {
+	m := ruleRegex.FindStringSubmatch(r.r[ruleIDx])
+	for _, g := range m[2:] {
+		v := strings.Split(g, "-")
+		if i >= intmath.Atoi(v[0]) && i <= intmath.Atoi(v[1]) {
+			return true
+		}
+	}
+	return false
+}
+
 type ticket struct {
 	val []int
 }
@@ -48,7 +58,64 @@ func part1(r rules, mt *ticket, ot []ticket) {
 	fmt.Printf("The sum of invalid fields is %v.\n", intmath.Sum(invalid...))
 }
 
+func validTickets(r rules, ot []ticket) (ret []ticket) {
+OUTER:
+	for _, t := range ot {
+		for _, i := range t.val {
+			if !r.isValid(i) {
+				continue OUTER
+			}
+		}
+		ret = append(ret, t)
+	}
+	return
+}
+
 func part2(r rules, mt *ticket, ot []ticket) {
+	columnToRule := map[int]map[int]int{}
+	for i := range r.r {
+		columnToRule[i] = map[int]int{}
+		for j := range r.r {
+			columnToRule[i][j] = j
+		}
+	}
+
+	vt := validTickets(r, ot)
+	for _, t := range vt {
+		for c, v := range t.val {
+			for ruleID := range r.r {
+				if !r.validForRule(ruleID, v) {
+					delete(columnToRule[c], ruleID)
+				}
+			}
+		}
+	}
+
+	uniquePairs := map[int]int{}
+	for len(columnToRule) > 0 {
+		for k, v := range columnToRule {
+			if len(v) == 1 {
+				for k2, v2 := range v {
+					uniquePairs[k] = v2
+					for _, v3 := range columnToRule {
+						delete(v3, k2)
+					}
+				}
+				delete(columnToRule, k)
+			}
+		}
+	}
+
+	ruleToColumn := map[int]int{}
+	for k, v := range uniquePairs {
+		ruleToColumn[v] = k
+	}
+
+	total := 1
+	for i := 0; i < 6; i++ {
+		total *= mt.val[ruleToColumn[i]]
+	}
+	fmt.Printf("The product of departure values is %v.\n", total)
 }
 
 func main() {
