3c73b2609a1f5d345b6e73e1ed991b045750f7f6
James Bogosian <bogosj@gmail.com>
2020-12-22 12:07:15 -0500

    Day 21 done

--- 2020/day21/main.go
+++ 2020/day21/main.go
@@ -2,15 +2,93 @@ package main
 
 import (
 	"fmt"
+	"regexp"
+	"sort"
+	"strings"
 	"time"
 
+	"github.com/albrow/stringset"
 	"github.com/bogosj/advent-of-code/fileinput"
 )
 
-func part1(in []string) {
+var (
+	foodReg = regexp.MustCompile(`(.*) \(contains (.*),?\)`)
+)
+
+type food struct {
+	data string
 }
 
-func part2(in []string) {
+func (f food) ingredients() []string {
+	return strings.Fields(foodReg.FindStringSubmatch(f.data)[1])
+}
+
+func (f food) allergens() []string {
+	return strings.Split(foodReg.FindStringSubmatch(f.data)[2], ", ")
+}
+
+func getAllergenMap(in []food) map[string]string {
+	allAllergens := stringset.New()
+	for _, f := range in {
+		allAllergens.Add(f.allergens()...)
+	}
+
+	allergenMap := map[string]string{}
+
+	for len(allAllergens) > 0 {
+		for a := range allAllergens {
+			ingredients := []stringset.Set{}
+			for _, f := range in {
+				currrentAllergens := stringset.NewFromSlice(f.allergens())
+				if currrentAllergens.Contains(a) {
+					currentIngredients := stringset.NewFromSlice(f.ingredients())
+					for k := range allergenMap {
+						currentIngredients.Remove(k)
+					}
+					ingredients = append(ingredients, currentIngredients)
+				}
+			}
+
+			s1 := ingredients[0]
+			for _, s2 := range ingredients[1:] {
+				s1 = stringset.Intersect(s1, s2)
+			}
+			if len(s1) == 1 {
+				allergenMap[s1.Slice()[0]] = a
+				allAllergens.Remove(a)
+				break
+			}
+		}
+	}
+	return allergenMap
+}
+
+func part1(in []food) {
+	allergenMap := getAllergenMap(in)
+	count := 0
+	for _, f := range in {
+		for _, i := range f.ingredients() {
+			if _, ok := allergenMap[i]; !ok {
+				count++
+			}
+		}
+	}
+	fmt.Printf("The non-allergen ingredients appear %v times\n", count)
+}
+
+func part2(in []food) {
+	allergenMap := getAllergenMap(in)
+	inv := map[string]string{}
+	allergens := []string{}
+	for k, v := range allergenMap {
+		inv[v] = k
+		allergens = append(allergens, v)
+	}
+	sort.Strings(allergens)
+	for _, v := range allergens {
+		fmt.Printf("%v,", inv[v])
+	}
+	fmt.Println()
 }
 
 func main() {
@@ -24,11 +102,11 @@ func main() {
 	fmt.Println("Part 2 done in", time.Since(start))
 }
 
-func input() []string {
-	ret := []string{}
+func input() []food {
+	ret := []food{}
 
 	for _, line := range fileinput.ReadLines("input.txt") {
-		ret = append(ret, line)
+		ret = append(ret, food{data: line})
 	}
 
 	return ret
