b655cf59b4b4c3685495acacd832d1b79ee5e192
James Bogosian <bogosj@gmail.com>
2020-12-24 11:28:30 -0500

    Rework to use array, day 23 complete.

--- 2020/day23/main.go
+++ 2020/day23/main.go
@@ -3,54 +3,78 @@ package main
 import (
 	"fmt"
 	"time"
-
-	"github.com/gammazero/deque"
 )
 
-func playRound(d *deque.Deque) {
-	dest := d.Front().(int) - 1
+// Returns the next cup
+func playRound(cups []int, curr, max int) (ret int) {
+	// Extract next three cups
+	a := cups[curr]
+	b := cups[a]
+	c := cups[b]
+	cups[curr] = cups[c]
+
+	// Return the next cup later
+	ret = cups[c]
+
+	// Get destination cup
+	dest := curr - 1
 	if dest == 0 {
-		dest = 9
+		dest = max
 	}
-	d.Rotate(1)
-	a, b, c := d.PopFront(), d.PopFront(), d.PopFront()
 	for dest == a || dest == b || dest == c {
 		dest--
 		if dest == 0 {
-			dest = 9
+			dest = max
 		}
 	}
-	rot := 1
-	for dest != d.Front() {
-		rot++
-		d.Rotate(1)
-	}
-	d.Rotate(1)
-	d.PushFront(c)
-	d.PushFront(b)
-	d.PushFront(a)
-	d.Rotate(-1 * rot)
+
+	// Insert a/b/c after destination
+	end := cups[dest]
+	cups[dest] = a
+	cups[c] = end
+	return
 }
 
-func finalState(d *deque.Deque) (ret string) {
-	for d.Front() != 1 {
-		d.Rotate(1)
+func makeCups(in []int) []int {
+	ret := make([]int, len(in)+1)
+	for idx, i := range in {
+		ret[i] = in[(idx+1)%len(in)]
 	}
-	d.PopFront()
-	for d.Len() > 0 {
-		ret += fmt.Sprint(d.PopFront())
+	return ret
+}
+
+func part1(in []int) {
+	m := makeCups(in)
+	curr := 3
+	for i := 0; i < 100; i++ {
+		curr = playRound(m, curr, 9)
+	}
+	next := m[1]
+	for next != 1 {
+		fmt.Print(next)
+		next = m[next]
+	}
+	fmt.Println()
+}
+
+func expand(in []int) (ret []int) {
+	ret = append(ret, in...)
+	for i := 10; i <= 1_000_000; i++ {
+		ret = append(ret, i)
 	}
 	return
 }
 
-func part1(in *deque.Deque) {
-	for i := 0; i < 100; i++ {
-		playRound(in)
+func part2(in []int) {
+	in = expand(in)
+	m := makeCups(in)
+	curr := 3
+	for i := 0; i < 10_000_000; i++ {
+		curr = playRound(m, curr, 1_000_000)
 	}
-	fmt.Println(finalState(in))
-}
-
-func part2(in *deque.Deque) {
+	a := m[1]
+	b := m[a]
+	fmt.Println(a * b)
 }
 
 func main() {
@@ -64,12 +88,6 @@ func main() {
 	fmt.Println("Part 2 done in", time.Since(start))
 }
 
-func input() *deque.Deque {
-	ret := deque.Deque{}
-	// Example data:
-	// for _, i := range []int{3, 8, 9, 1, 2, 5, 4, 6, 7} {
-	for _, i := range []int{3, 1, 5, 6, 7, 9, 8, 2, 4} {
-		ret.PushBack(i)
-	}
-	return &ret
+func input() []int {
+	return []int{3, 1, 5, 6, 7, 9, 8, 2, 4}
 }
