7b97aafd9d6c4b5fd9bcf709bb2c3fc8f4daec1a
James Bogosian <bogosj@gmail.com>
2020-12-23 20:25:15 -0500

    Day 23 part 1 done.

--- 2020/day23/main.go
+++ 2020/day23/main.go
@@ -4,13 +4,53 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/bogosj/advent-of-code/fileinput"
+	"github.com/gammazero/deque"
 )
 
-func part1(in []string) {
+func playRound(d *deque.Deque) {
+	dest := d.Front().(int) - 1
+	if dest == 0 {
+		dest = 9
+	}
+	d.Rotate(1)
+	a, b, c := d.PopFront(), d.PopFront(), d.PopFront()
+	for dest == a || dest == b || dest == c {
+		dest--
+		if dest == 0 {
+			dest = 9
+		}
+	}
+	rot := 1
+	for dest != d.Front() {
+		rot++
+		d.Rotate(1)
+	}
+	d.Rotate(1)
+	d.PushFront(c)
+	d.PushFront(b)
+	d.PushFront(a)
+	d.Rotate(-1 * rot)
 }
 
-func part2(in []string) {
+func finalState(d *deque.Deque) (ret string) {
+	for d.Front() != 1 {
+		d.Rotate(1)
+	}
+	d.PopFront()
+	for d.Len() > 0 {
+		ret += fmt.Sprint(d.PopFront())
+	}
+	return
+}
+
+func part1(in *deque.Deque) {
+	for i := 0; i < 100; i++ {
+		playRound(in)
+	}
+	fmt.Println(finalState(in))
+}
+
+func part2(in *deque.Deque) {
 }
 
 func main() {
@@ -24,12 +64,12 @@ func main() {
 	fmt.Println("Part 2 done in", time.Since(start))
 }
 
-func input() []string {
-	ret := []string{}
-
-	for _, line := range fileinput.ReadLines("input.txt") {
-		ret = append(ret, line)
+func input() *deque.Deque {
+	ret := deque.Deque{}
+	// Example data:
+	// for _, i := range []int{3, 8, 9, 1, 2, 5, 4, 6, 7} {
+	for _, i := range []int{3, 1, 5, 6, 7, 9, 8, 2, 4} {
+		ret.PushBack(i)
 	}
-
-	return ret
+	return &ret
 }
