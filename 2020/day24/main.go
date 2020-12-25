305e764cf4b086693ac4634f8815010a2b104b72
James Bogosian <bogosj@gmail.com>
2020-12-24 22:26:54 -0500

    Day 24 complete.

--- 2020/day24/main.go
+++ 2020/day24/main.go
@@ -11,6 +11,13 @@ type hexPoint struct {
 	x, y, z int
 }
 
+func (h *hexPoint) neighbors() []hexPoint {
+	return []hexPoint{
+		{h.x + 1, h.y - 1, h.z}, {h.x + 1, h.y, h.z - 1}, {h.x, h.y + 1, h.z - 1},
+		{h.x - 1, h.y + 1, h.z}, {h.x - 1, h.y, h.z + 1}, {h.x, h.y - 1, h.z + 1},
+	}
+}
+
 type directions struct {
 	d string
 }
@@ -45,12 +52,17 @@ func (d *directions) endPoint() (ret hexPoint) {
 	return
 }
 
-func part1(in []directions) {
-	m := map[hexPoint]bool{}
+func startingTiles(in []directions) map[hexPoint]bool {
+	ret := map[hexPoint]bool{}
 	for _, d := range in {
 		p := d.endPoint()
-		m[p] = !m[p]
+		ret[p] = !ret[p]
 	}
+	return ret
+}
+
+func part1(in []directions) {
+	m := startingTiles(in)
 	count := 0
 	for _, v := range m {
 		if v {
@@ -60,7 +72,61 @@ func part1(in []directions) {
 	fmt.Printf("There are %d black tiles\n", count)
 }
 
+func countNeighbors(p hexPoint, state map[hexPoint]bool) (black int) {
+	for _, n := range p.neighbors() {
+		if state[n] {
+			black++
+		}
+	}
+	return
+}
+
+func step(state map[hexPoint]bool) map[hexPoint]bool {
+	ret := map[hexPoint]bool{}
+
+	// Flip blacks to white.
+	for p, black := range state {
+		if black {
+			nBlack := countNeighbors(p, state)
+			if nBlack == 0 || nBlack > 2 {
+				ret[p] = false
+			} else {
+				ret[p] = true
+			}
+		}
+	}
+
+	// Find neighbor whites of exactly two blacks and flip them black.
+	whitesWithBlack := map[hexPoint]int{}
+	for p, black := range state {
+		if black {
+			for _, np := range p.neighbors() {
+				if !state[np] {
+					whitesWithBlack[np]++
+				}
+			}
+		}
+	}
+	for p, c := range whitesWithBlack {
+		if c == 2 {
+			ret[p] = true
+		}
+	}
+	return ret
+}
+
 func part2(in []directions) {
+	m := startingTiles(in)
+	for i := 0; i < 100; i++ {
+		m = step(m)
+	}
+	c := 0
+	for _, black := range m {
+		if black {
+			c++
+		}
+	}
+	fmt.Printf("After 100 days there will be %v black tiles\n", c)
 }
 
 func main() {
