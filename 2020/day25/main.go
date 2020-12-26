4924677c94d7e740e8b33baf6c6de836e6ddc58a
James Bogosian <bogosj@gmail.com>
2020-12-26 12:57:54 -0500

    Day 25 part 1 done

--- 2020/day25/main.go
+++ 2020/day25/main.go
@@ -3,33 +3,37 @@ package main
 import (
 	"fmt"
 	"time"
-
-	"github.com/bogosj/advent-of-code/fileinput"
 )
 
-func part1(in []string) {
+const (
+	key1 = 10441485
+	key2 = 1004920
+	sn   = 7
+)
+
+func part1() {
+	v, loopSize := 1, 0
+	for v != key1 {
+		v *= sn
+		v %= 20201227
+		loopSize++
+	}
+	v = 1
+	for j := 0; j < loopSize; j++ {
+		v *= key2
+		v %= 20201227
+	}
+	fmt.Printf("The encryption key is %v\n", v)
 }
 
-func part2(in []string) {
+func part2() {
 }
 
 func main() {
-	in := input()
 	start := time.Now()
-	part1(in)
+	part1()
 	fmt.Println("Part 1 done in", time.Since(start))
-	in = input()
 	start = time.Now()
-	part2(in)
+	part2()
 	fmt.Println("Part 2 done in", time.Since(start))
-}
-
-func input() []string {
-	ret := []string{}
-
-	for _, line := range fileinput.ReadLines("input.txt") {
-		ret = append(ret, line)
-	}
-
-	return ret
 }
