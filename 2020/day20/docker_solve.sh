f21b2de5be2d59ddb93af045db789a785be5ba89
James Bogosian <bogosj@gmail.com>
2020-12-27 17:49:19 -0500

    Disable wget output.

--- 2020/day20/docker_solve.sh
+++ 2020/day20/docker_solve.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 cd /tmp
-wget https://raw.githubusercontent.com/AidanGlickman/Advent-2020/4cc3efc1a5a6a6e7ab0c0bc26b4d5ce60cfa77e0/day20/solution.py
+wget -q https://raw.githubusercontent.com/AidanGlickman/Advent-2020/4cc3efc1a5a6a6e7ab0c0bc26b4d5ce60cfa77e0/day20/solution.py
 cp /work/input.txt /tmp
 python3 solution.py
