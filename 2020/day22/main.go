ed6726f24c9ee5d7d3134ff05396150cfcd77a3f
James Bogosian <bogosj@gmail.com>
2020-12-27 16:27:13 -0500

    Day 22 done

--- 2020/day22/main.go
+++ 2020/day22/main.go
@@ -13,6 +13,19 @@ type deck struct {
 	cards []int
 }
 
+func (d *deck) String() (ret string) {
+	for _, i := range d.cards {
+		ret += fmt.Sprintf("%d,", i)
+	}
+	return
+}
+
+func (d *deck) dupe(c int) *deck {
+	ret := deck{}
+	ret.cards = append(ret.cards, d.cards[:c]...)
+	return &ret
+}
+
 func (d *deck) score() int {
 	score := 0
 	for i, mult := 0, len(d.cards); mult > 0; i++ {
@@ -40,7 +53,42 @@ func part1(in []*deck) {
 	fmt.Printf("The winner's score is %v\n", winner.score())
 }
 
+func playSubGame(in []*deck) int {
+	states := map[string]bool{}
+	for len(in[0].cards) > 0 && len(in[1].cards) > 0 {
+		state := fmt.Sprintf("%s|%s", in[0], in[1])
+		if states[state] {
+			return 0
+		}
+		states[state] = true
+		p1 := in[0].cards[0]
+		p2 := in[1].cards[0]
+		in[0].cards = in[0].cards[1:]
+		in[1].cards = in[1].cards[1:]
+		winner := 0
+		if len(in[0].cards) >= p1 && len(in[1].cards) >= p2 {
+			winner = playSubGame([]*deck{in[0].dupe(p1), in[1].dupe(p2)})
+		} else {
+			if p2 > p1 {
+				winner = 1
+			}
+		}
+		if winner == 0 {
+			in[0].cards = append(in[0].cards, p1, p2)
+		} else {
+			in[1].cards = append(in[1].cards, p2, p1)
+		}
+	}
+
+	if len(in[0].cards) == 0 {
+		return 1
+	}
+	return 0
+}
+
 func part2(in []*deck) {
+	winner := playSubGame(in)
+	fmt.Printf("The winner of recursive's score is %d\n", in[winner].score())
 }
 
 func main() {
