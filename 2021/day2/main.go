1495a4d71beba55a6aaa871e9fdeb01f2041fd29
James Bogosian <bogosj@gmail.com>
2021-12-02 09:00:49 -0500

    Day 2 part 1 done.

--- 2021/day2/main.go
+++ 2021/day2/main.go
@@ -2,12 +2,27 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/bogosj/advent-of-code/fileinput"
+	"github.com/bogosj/advent-of-code/intmath"
 )
 
 func part1(in []string) {
+	pos := intmath.Point{}
+	for _, inst := range in {
+		f := strings.Fields(inst)
+		switch f[0] {
+		case "forward":
+			pos.X += intmath.Atoi(f[1])
+		case "up":
+			pos.Y -= intmath.Atoi(f[1])
+		case "down":
+			pos.Y += intmath.Atoi(f[1])
+		}
+	}
+	fmt.Println("Part 1 answer:", pos.X*pos.Y)
 }
 
 func part2(in []string) {
