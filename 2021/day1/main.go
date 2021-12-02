5a192f431946541639c7ff88bcd0e0897f878291
James Bogosian <bogosj@gmail.com>
2021-12-01 20:28:39 -0500

    Day 1 part 2 done.

--- 2021/day1/main.go
+++ 2021/day1/main.go
@@ -21,6 +21,20 @@ func part1(in []string) {
 }
 
 func part2(in []string) {
+	i := 0
+	count := 0
+	for {
+		i2 := intmath.Atoi(in[0]) + intmath.Atoi(in[1]) + intmath.Atoi(in[2])
+		if i2 > i {
+			count++
+		}
+		in = in[1:]
+		if len(in) < 3 {
+			break
+		}
+		i = i2
+	}
+	fmt.Println("Part 2 answer:", count-1)
 }
 
 func main() {
