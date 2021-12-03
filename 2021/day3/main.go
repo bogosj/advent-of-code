6181501ba7c55ef752bd3f26b50d99af240f4d0b
James Bogosian <bogosj@gmail.com>
2021-12-03 09:51:09 -0500

    Day 3 done

--- 2021/day3/main.go
+++ 2021/day3/main.go
@@ -2,15 +2,84 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"time"
 
 	"github.com/bogosj/advent-of-code/fileinput"
+	"github.com/bogosj/advent-of-code/intmath"
 )
 
 func part1(in []string) {
+	m := map[int]int{}
+	for _, line := range in {
+		for i := 0; i < 12; i++ {
+			m[i] += int(line[i]) - 48
+		}
+	}
+	g := ""
+	e := ""
+	for i := 0; i < 12; i++ {
+		if m[i] > len(in)/2 {
+			g += "1"
+			e += "0"
+		} else {
+			g += "0"
+			e += "1"
+		}
+	}
+	gi, _ := strconv.ParseInt(g, 2, 64)
+	ei, _ := strconv.ParseInt(e, 2, 64)
+	fmt.Println("Part 1 answer:", gi*ei)
+}
+
+func filterReadings(in []string, most bool) []string {
+	for i := 0; i < 12; i++ {
+		next := []string{}
+		count := 0
+		for _, line := range in {
+			count += int(line[i]) - 48
+		}
+		searchBit := '0'
+		if count < (len(in)+1)/2 {
+			if most {
+				searchBit = '0'
+			} else {
+				searchBit = '1'
+			}
+		} else {
+			if most {
+				searchBit = '1'
+			} else {
+				searchBit = '0'
+			}
+		}
+		for _, line := range in {
+			if rune(line[i]) == searchBit {
+				next = append(next, line)
+			}
+		}
+		in = next
+		if len(in) == 1 {
+			return in
+		}
+	}
+	return in
 }
 
 func part2(in []string) {
+	m := map[int]int{}
+	for _, line := range in {
+		for i, c := range line {
+			m[i] += intmath.Atoi(string(c))
+		}
+	}
+
+	oxygen := filterReadings(in, true)
+	co2 := filterReadings(in, false)
+
+	oReading, _ := strconv.ParseInt(oxygen[0], 2, 64)
+	co2Reading, _ := strconv.ParseInt(co2[0], 2, 64)
+	fmt.Println("Part 2 answer:", oReading*co2Reading)
 }
 
 func main() {
@@ -25,11 +94,5 @@ func main() {
 }
 
 func input() []string {
-	ret := []string{}
-
-	for _, line := range fileinput.ReadLines("input.txt") {
-		ret = append(ret, line)
-	}
-
-	return ret
+	return fileinput.ReadLines("input.txt")
 }
