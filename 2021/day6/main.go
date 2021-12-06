30d83f377380876a4e5ac4bb0b30ba6cb5041ea4
James Bogosian <bogosj@gmail.com>
2021-12-06 09:52:35 -0500

    Day 6 part 1 done.

--- 2021/day6/main.go
+++ 2021/day6/main.go
@@ -3,14 +3,35 @@ package main
 import (
 	"fmt"
 	"time"
-
-	"github.com/bogosj/advent-of-code/fileinput"
 )
 
-func part1(in []string) {
+func makeCounter(in []int) map[int]int {
+	ret := map[int]int{}
+	for _, i := range in {
+		ret[i]++
+	}
+	return ret
 }
 
-func part2(in []string) {
+func part1(in []int) {
+	counter := makeCounter(in)
+	for i := 0; i < 80; i++ {
+		newCounter := map[int]int{}
+		for i := 1; i <= 8; i++ {
+			newCounter[i-1] = counter[i]
+		}
+		newCounter[8] = counter[0]
+		newCounter[6] += counter[0]
+		counter = newCounter
+	}
+	sum := 0
+	for _, v := range counter {
+		sum += v
+	}
+	fmt.Println("Part 1 answer:", sum)
+}
+
+func part2(in []int) {
 }
 
 func main() {
@@ -24,12 +45,7 @@ func main() {
 	fmt.Println("Part 2 done in", time.Since(start))
 }
 
-func input() []string {
-	ret := []string{}
-
-	for _, line := range fileinput.ReadLines("input.txt") {
-		ret = append(ret, line)
-	}
-
-	return ret
+func input() []int {
+	//return []int{3, 4, 3, 1, 2}
+	return []int{2, 4, 1, 5, 1, 3, 1, 1, 5, 2, 2, 5, 4, 2, 1, 2, 5, 3, 2, 4, 1, 3, 5, 3, 1, 3, 1, 3, 5, 4, 1, 1, 1, 1, 5, 1, 2, 5, 5, 5, 2, 3, 4, 1, 1, 1, 2, 1, 4, 1, 3, 2, 1, 4, 3, 1, 4, 1, 5, 4, 5, 1, 4, 1, 2, 2, 3, 1, 1, 1, 2, 5, 1, 1, 1, 2, 1, 1, 2, 2, 1, 4, 3, 3, 1, 1, 1, 2, 1, 2, 5, 4, 1, 4, 3, 1, 5, 5, 1, 3, 1, 5, 1, 5, 2, 4, 5, 1, 2, 1, 1, 5, 4, 1, 1, 4, 5, 3, 1, 4, 5, 1, 3, 2, 2, 1, 1, 1, 4, 5, 2, 2, 5, 1, 4, 5, 2, 1, 1, 5, 3, 1, 1, 1, 3, 1, 2, 3, 3, 1, 4, 3, 1, 2, 3, 1, 4, 2, 1, 2, 5, 4, 2, 5, 4, 1, 1, 2, 1, 2, 4, 3, 3, 1, 1, 5, 1, 1, 1, 1, 1, 3, 1, 4, 1, 4, 1, 2, 3, 5, 1, 2, 5, 4, 5, 4, 1, 3, 1, 4, 3, 1, 2, 2, 2, 1, 5, 1, 1, 1, 3, 2, 1, 3, 5, 2, 1, 1, 4, 4, 3, 5, 3, 5, 1, 4, 3, 1, 3, 5, 1, 3, 4, 1, 2, 5, 2, 1, 5, 4, 3, 4, 1, 3, 3, 5, 1, 1, 3, 5, 3, 3, 4, 3, 5, 5, 1, 4, 1, 1, 3, 5, 5, 1, 5, 4, 4, 1, 3, 1, 1, 1, 1, 3, 2, 1, 2, 3, 1, 5, 1, 1, 1, 4, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 5, 3}
 }
