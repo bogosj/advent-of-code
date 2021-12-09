74a4803cd301152a03ebaa1036469a1ddc5dcc43
James Bogosian <bogosj@gmail.com>
2021-12-09 09:31:36 -0500

    Day 9 part 1 done.

--- 2021/day9/main.go
+++ 2021/day9/main.go
@@ -5,12 +5,25 @@ import (
 	"time"
 
 	"github.com/bogosj/advent-of-code/fileinput"
+	"github.com/bogosj/advent-of-code/intmath"
 )
 
-func part1(in []string) {
+func part1(in map[intmath.Point]int) {
+	risk := 0
+OUTER:
+	for k, v := range in {
+		for _, n := range k.Neighbors() {
+			nv, ok := in[n]
+			if nv <= v && ok {
+				continue OUTER
+			}
+		}
+		risk += v + 1
+	}
+	fmt.Println("Part 1 answer:", risk)
 }
 
-func part2(in []string) {
+func part2(in map[intmath.Point]int) {
 }
 
 func main() {
@@ -24,11 +37,13 @@ func main() {
 	fmt.Println("Part 2 done in", time.Since(start))
 }
 
-func input() []string {
-	ret := []string{}
+func input() map[intmath.Point]int {
+	ret := map[intmath.Point]int{}
 
-	for _, line := range fileinput.ReadLines("input.txt") {
-		ret = append(ret, line)
+	for y, line := range fileinput.ReadLines("input.txt") {
+		for x, char := range line {
+			ret[intmath.Point{X: x, Y: y}] = intmath.Atoi(string(char))
+		}
 	}
 
 	return ret
