ba23e58d828bcfe980fe13bdfee68e00a3b5c5d4
James Bogosian <bogosj@gmail.com>
2021-12-09 14:42:56 -0500

    Day 8 part 2 done.

--- 2021/day8/main.go
+++ 2021/day8/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sort"
 	"strings"
 	"time"
 
@@ -21,7 +22,97 @@ func part1(in []string) {
 	fmt.Println("Part 1 answer:", count)
 }
 
+func sortString(w string) string {
+	s := strings.Split(w, "")
+	sort.Strings(s)
+	return strings.Join(s, "")
+}
+
+func selector(wires []string, length int) (ret []string) {
+	for _, w := range wires {
+		if len(w) == length {
+			ret = append(ret, w)
+		}
+	}
+	return
+}
+
+func finder(s string, possibles []string, inCommon int) string {
+	for _, os := range possibles {
+		common := map[rune]int{}
+		for _, c := range s {
+			common[c]++
+		}
+		for _, c := range os {
+			common[c]++
+		}
+		count := 0
+		for _, v := range common {
+			if v > 1 {
+				count++
+			}
+		}
+		if count == inCommon {
+			return os
+		}
+	}
+	return ""
+}
+
+func findThird(values map[string]int, wires []string, v int) {
+	for _, wire := range wires {
+		_, ok := values[wire]
+		if !ok {
+			values[wire] = v
+		}
+	}
+}
+
+func findDigits(wires []string) map[string]int {
+	numToString := map[int]string{}
+	numToString[1] = selector(wires, 2)[0]
+	numToString[7] = selector(wires, 3)[0]
+	numToString[4] = selector(wires, 4)[0]
+	numToString[8] = selector(wires, 7)[0]
+	numToString[2] = finder(numToString[4], selector(wires, 5), 2)
+	numToString[3] = finder(numToString[1], selector(wires, 5), 2)
+	numToString[6] = finder(numToString[1], selector(wires, 6), 1)
+	numToString[9] = finder(numToString[4], selector(wires, 6), 4)
+
+	ret := map[string]int{}
+	for k, v := range numToString {
+		ret[v] = k
+	}
+	findThird(ret, selector(wires, 5), 5)
+	findThird(ret, selector(wires, 6), 0)
+	return ret
+}
+
+func decodeLine(line string) int {
+	f := strings.Split(line, " | ")
+	wires := strings.Fields(f[0])
+	display := strings.Fields(f[1])
+	for i := 0; i < len(wires); i++ {
+		wires[i] = sortString(wires[i])
+	}
+	for i := 0; i < len(display); i++ {
+		display[i] = sortString(display[i])
+	}
+	digits := findDigits(wires)
+	sum := 0
+	for _, digit := range display {
+		sum *= 10
+		sum += digits[digit]
+	}
+	return sum
+}
+
 func part2(in []string) {
+	sum := 0
+	for _, line := range in {
+		sum += decodeLine(line)
+	}
+	fmt.Println("Part 2 answer:", sum)
 }
 
 func main() {
@@ -36,11 +127,5 @@ func main() {
 }
 
 func input() []string {
-	ret := []string{}
-
-	for _, line := range fileinput.ReadLines("input.txt") {
-		ret = append(ret, line)
-	}
-
-	return ret
+	return fileinput.ReadLines("input.txt")
 }
