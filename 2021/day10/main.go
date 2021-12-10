19f8600a2e476a5313c2bfcc3769e714e58141cc
James Bogosian <bogosj@gmail.com>
2021-12-10 08:57:56 -0500

    Day 10 part 1 done.

--- 2021/day10/main.go
+++ 2021/day10/main.go
@@ -7,7 +7,49 @@ import (
 	"github.com/bogosj/advent-of-code/fileinput"
 )
 
+var closes = map[rune]rune{
+	')': '(',
+	'}': '{',
+	']': '[',
+	'>': '<',
+}
+
+func isOpen(char rune) bool {
+	switch char {
+	case '(', '{', '[', '<':
+		return true
+	default:
+		return false
+	}
+}
+
+func badChar(line string) rune {
+	chars := []rune{}
+	for _, c := range line {
+		if len(chars) > 0 {
+			if isOpen(c) {
+				chars = append(chars, c)
+			} else {
+				if chars[len(chars)-1] != closes[c] {
+					return c
+				} else {
+					chars = chars[:len(chars)-1]
+				}
+			}
+		} else {
+			chars = append(chars, c)
+		}
+	}
+	return 'x'
+}
+
 func part1(in []string) {
+	b := map[rune]int{}
+	for _, line := range in {
+		b[badChar(line)]++
+	}
+	score := b[')']*3 + b[']']*57 + b['}']*1197 + b['>']*25137
+	fmt.Println("Part 1 answer:", score)
 }
 
 func part2(in []string) {
@@ -25,11 +67,5 @@ func main() {
 }
 
 func input() []string {
-	ret := []string{}
-
-	for _, line := range fileinput.ReadLines("input.txt") {
-		ret = append(ret, line)
-	}
-
-	return ret
+	return fileinput.ReadLines("input.txt")
 }
