25c272c3de9c9dd0c88dc1dc228538bd0cc66cc8
James Bogosian <bogosj@gmail.com>
2021-12-13 09:19:27 -0500

    Day 13 part 1 done.

--- 2021/day13/main.go
+++ 2021/day13/main.go
@@ -2,12 +2,61 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/bogosj/advent-of-code/fileinput"
+	"github.com/bogosj/advent-of-code/intmath"
 )
 
+func markPoints(in []string) map[intmath.Point]bool {
+	ret := map[intmath.Point]bool{}
+	for _, line := range in {
+		if strings.HasPrefix(line, "fold") {
+			continue
+		}
+
+		f := strings.Split(line, ",")
+		if len(f) == 2 {
+			ret[intmath.Point{X: intmath.Atoi(f[0]), Y: intmath.Atoi(f[1])}] = true
+		}
+	}
+	return ret
+}
+
+func fold(paper map[intmath.Point]bool, dir string, coord int) {
+	for point := range paper {
+		if dir == "x" {
+			if point.X > coord {
+				newPoint := intmath.Point{Y: point.Y}
+				newPoint.X = coord - (point.X - coord)
+				paper[newPoint] = true
+				delete(paper, point)
+			}
+		} else {
+			if point.Y > coord {
+				newPoint := intmath.Point{X: point.X}
+				newPoint.Y = (2 * (point.Y - coord)) - coord
+				paper[newPoint] = true
+				delete(paper, point)
+			}
+		}
+	}
+}
+
 func part1(in []string) {
+	paper := markPoints(in)
+	fold(paper, "x", 655)
+	//fold(paper, "y", 7)
+
+	count := 0
+	for p, v := range paper {
+		if v {
+			count++
+			fmt.Println(p)
+		}
+	}
+	fmt.Println("Part 1 answer:", count)
 }
 
 func part2(in []string) {
@@ -25,11 +74,5 @@ func main() {
 }
 
 func input() []string {
-	ret := []string{}
-
-	for _, line := range fileinput.ReadLines("input.txt") {
-		ret = append(ret, line)
-	}
-
-	return ret
+	return fileinput.ReadLines("input.txt")
 }
