49f8999f96909073a84060262144d165d7ec0512
James Bogosian <bogosj@gmail.com>
2021-12-13 09:34:18 -0500

    Day 13 done.

--- 2021/day13/main.go
+++ 2021/day13/main.go
@@ -36,7 +36,7 @@ func fold(paper map[intmath.Point]bool, dir string, coord int) {
 		} else {
 			if point.Y > coord {
 				newPoint := intmath.Point{X: point.X}
-				newPoint.Y = (2 * (point.Y - coord)) - coord
+				newPoint.Y = coord - (point.Y - coord)
 				paper[newPoint] = true
 				delete(paper, point)
 			}
@@ -47,19 +47,49 @@ func fold(paper map[intmath.Point]bool, dir string, coord int) {
 func part1(in []string) {
 	paper := markPoints(in)
 	fold(paper, "x", 655)
-	//fold(paper, "y", 7)
 
 	count := 0
-	for p, v := range paper {
+	for _, v := range paper {
 		if v {
 			count++
-			fmt.Println(p)
 		}
 	}
 	fmt.Println("Part 1 answer:", count)
 }
 
+func printPaper(paper map[intmath.Point]bool) {
+	xs, ys := []int{}, []int{}
+	for p := range paper {
+		xs = append(xs, p.X)
+		ys = append(ys, p.Y)
+	}
+	for y := 0; y <= intmath.Max(ys...); y++ {
+		for x := 0; x <= intmath.Max(xs...); x++ {
+			if paper[intmath.Point{X: x, Y: y}] {
+				fmt.Print("#")
+			} else {
+				fmt.Print(" ")
+			}
+		}
+		fmt.Println()
+	}
+}
+
 func part2(in []string) {
+	paper := markPoints(in)
+	fold(paper, "x", 655)
+	fold(paper, "y", 447)
+	fold(paper, "x", 327)
+	fold(paper, "y", 223)
+	fold(paper, "x", 163)
+	fold(paper, "y", 111)
+	fold(paper, "x", 81)
+	fold(paper, "y", 55)
+	fold(paper, "x", 40)
+	fold(paper, "y", 27)
+	fold(paper, "y", 13)
+	fold(paper, "y", 6)
+	printPaper(paper)
 }
 
 func main() {
