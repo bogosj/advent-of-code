2721674540fc9f7379505a66110e37b82d83fb6e
James Bogosian <bogosj@gmail.com>
2021-12-15 11:32:50 -0500

    Day 15 part 1 done.

--- 2021/day15/main.go
+++ 2021/day15/main.go
@@ -6,9 +6,57 @@ import (
 
 	"github.com/bogosj/advent-of-code/fileinput"
 	"github.com/bogosj/advent-of-code/intmath"
+	pq "github.com/jupp0r/go-priority-queue"
 )
 
+type state struct {
+	pos  intmath.Point
+	risk int
+}
+
+func findExit(ceiling map[intmath.Point]int) intmath.Point {
+	xs, ys := []int{}, []int{}
+	for point := range ceiling {
+		xs = append(xs, point.X)
+		ys = append(ys, point.Y)
+	}
+	return intmath.Point{X: intmath.Max(xs...), Y: intmath.Max(ys...)}
+}
+
+func optimalPath(ceiling map[intmath.Point]int) *state {
+	exit := findExit(ceiling)
+	states := pq.New()
+	states.Insert(&state{}, 0)
+	visited := map[intmath.Point]bool{}
+	visited[intmath.Point{}] = true
+
+	for states.Len() > 0 {
+		i, err := states.Pop()
+		if err != nil {
+			panic(err)
+		}
+		curr := i.(*state)
+		if curr.pos.ManhattanDistanceTo(exit) == 0 {
+			return curr
+		}
+		for _, n := range curr.pos.Neighbors() {
+			if visited[n] {
+				continue
+			}
+			risk, ok := ceiling[n]
+			if ok {
+				ns := &state{pos: n, risk: curr.risk + risk}
+				states.Insert(ns, float64(ns.risk))
+				visited[ns.pos] = true
+			}
+		}
+	}
+	return &state{}
+}
+
 func part1(ceiling map[intmath.Point]int) {
+	s := optimalPath(ceiling)
+	fmt.Println("Part 1 answer:", s.risk)
 }
 
 func part2(ceiling map[intmath.Point]int) {
