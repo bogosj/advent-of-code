3f162c3b62b6cf9f98414ccbdc3a418b717d4bf8
James Bogosian <bogosj@gmail.com>
2021-12-19 16:31:47 -0500

    Parsing code for scanners.

--- 2021/day19/main.go
+++ 2021/day19/main.go
@@ -2,12 +2,50 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/bogosj/advent-of-code/fileinput"
+	"github.com/bogosj/advent-of-code/intmath"
 )
 
+type scanner struct {
+	points [][]int
+}
+
+func (s *scanner) addPoints(line string) {
+	sps := strings.Split(line, ",")
+	p := make([]int, 3)
+	for i, sp := range sps {
+		p[i] = intmath.Atoi(sp)
+	}
+	s.points = append(s.points, p)
+}
+
+func buildScanners(in []string) []scanner {
+	ret := []scanner{}
+
+	s := scanner{}
+	for i, line := range in {
+		if strings.Contains(line, "scanner") {
+			if i != 0 {
+				ret = append(ret, s)
+			}
+			s = scanner{}
+			continue
+		}
+		if len(line) > 0 {
+			s.addPoints(line)
+		}
+	}
+	ret = append(ret, s)
+
+	return ret
+}
+
 func part1(in []string) {
+	s := buildScanners(in)
+	fmt.Println(s)
 }
 
 func part2(in []string) {
