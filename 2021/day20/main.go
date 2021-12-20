fb986aa7799c00ce12f0e25a0783dc195ff77c89
James Bogosian <bogosj@gmail.com>
2021-12-20 10:49:04 -0500

    Day 20 shell

---
+++ 2021/day20/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/bogosj/advent-of-code/fileinput"
+)
+
+func part1(in []string) {
+}
+
+func part2(in []string) {
+}
+
+func main() {
+	in := input()
+	start := time.Now()
+	part1(in)
+	fmt.Println("Part 1 done in", time.Since(start))
+	in = input()
+	start = time.Now()
+	part2(in)
+	fmt.Println("Part 2 done in", time.Since(start))
+}
+
+func input() []string {
+	return fileinput.ReadLines("input.txt")
+}
