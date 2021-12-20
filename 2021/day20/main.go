ae2c8774080803bc5a5713a0c57b208f57bb6ada
James Bogosian <bogosj@gmail.com>
2021-12-20 16:28:24 -0500

    Day 20 part 1 done.

--- 2021/day20/main.go
+++ 2021/day20/main.go
@@ -31,7 +31,48 @@ func processInput(in []string) ([]bool, map[intmath.Point]bool) {
 	return algo, image
 }
 
+func runAlgo(algo []bool, image map[intmath.Point]bool, p intmath.Point) bool {
+	val := 0
+	for y := p.Y - 1; y <= p.Y+1; y++ {
+		for x := p.X - 1; x <= p.X+1; x++ {
+			np := intmath.Point{X: x, Y: y}
+			val = val << 1
+			if image[np] {
+				val = val + 1
+			}
+		}
+	}
+	return algo[val]
+}
+
+func step(algo []bool, image map[intmath.Point]bool) map[intmath.Point]bool {
+	newImage := map[intmath.Point]bool{}
+
+	for y := 0; y <= 300; y++ {
+		for x := 0; x <= 300; x++ {
+			p := intmath.Point{X: x, Y: y}
+			if y == 0 || x == 0 || y == 300 || x == 300 {
+				newImage[p] = !image[p]
+			} else {
+				newImage[p] = runAlgo(algo, image, p)
+			}
+		}
+	}
+
+	return newImage
+}
+
 func part1(in []string) {
+	algo, image := processInput(in)
+	image = step(algo, image)
+	image = step(algo, image)
+	count := 0
+	for _, c := range image {
+		if c {
+			count++
+		}
+	}
+	fmt.Println("Part 1 answer:", count)
 }
 
 func part2(in []string) {
