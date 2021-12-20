157e34781ed9c702547b002f5eac5424620a3c64
James Bogosian <bogosj@gmail.com>
2021-12-20 12:03:31 -0500

    Process day 20 input.

--- 2021/day20/main.go
+++ 2021/day20/main.go
@@ -5,7 +5,31 @@ import (
 	"time"
 
 	"github.com/bogosj/advent-of-code/fileinput"
+	"github.com/bogosj/advent-of-code/intmath"
 )
+
+func processInput(in []string) ([]bool, map[intmath.Point]bool) {
+	algo := []bool{}
+	image := map[intmath.Point]bool{}
+
+	for _, c := range in[0] {
+		if c == '#' {
+			algo = append(algo, true)
+		} else {
+			algo = append(algo, false)
+		}
+	}
+
+	for y, line := range in[2:] {
+		for x, c := range line {
+			if c == '#' {
+				image[intmath.Point{X: x + 100, Y: y + 100}] = true
+			}
+		}
+	}
+
+	return algo, image
+}
 
 func part1(in []string) {
 }
