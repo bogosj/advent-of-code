dfed3e99a584c9d1b9f4b478f6fff2e7451b1399
James Bogosian <bogosj@gmail.com>
2021-12-21 14:51:19 -0500

    Day 21 part 1 complete.

--- 2021/day21/main.go
+++ 2021/day21/main.go
@@ -4,26 +4,67 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/bogosj/advent-of-code/fileinput"
+	"github.com/bogosj/advent-of-code/intmath"
 )
 
-func part1(in []string) {
+/*
+Player 1 starting position: 7
+Player 2 starting position: 10
+*/
+
+type player struct {
+	position, score int
 }
 
-func part2(in []string) {
+func (p player) String() string {
+	return fmt.Sprintf("Position: %d, Score: %d", p.position, p.score)
+}
+
+// takeTurn returns the state of the die after the player has taken their turn.
+func (p *player) takeTurn(die int) int {
+	for i := 0; i < 3; i++ {
+		p.position += die
+		for p.position > 10 {
+			p.position -= 10
+		}
+		die++
+		if die == 101 {
+			die = 1
+		}
+	}
+	return die
+}
+
+func part1() {
+	player1 := player{position: 7}
+	player2 := player{position: 10}
+	die, rolls := 1, 0
+	for {
+		die = player1.takeTurn(die)
+		rolls += 3
+		player1.score += player1.position
+		if player1.score >= 1000 {
+			break
+		}
+
+		die = player2.takeTurn(die)
+		rolls += 3
+		player2.score += player2.position
+		if player2.score >= 1000 {
+			break
+		}
+	}
+	fmt.Println("Part 1 answer:", intmath.Min(player1.score, player2.score)*rolls)
+}
+
+func part2() {
 }
 
 func main() {
-	in := input()
 	start := time.Now()
-	part1(in)
+	part1()
 	fmt.Println("Part 1 done in", time.Since(start))
-	in = input()
 	start = time.Now()
-	part2(in)
+	part2()
 	fmt.Println("Part 2 done in", time.Since(start))
-}
-
-func input() []string {
-	return fileinput.ReadLines("input.txt")
 }
