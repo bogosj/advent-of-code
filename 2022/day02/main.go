a0c197ca4a090b534f7ba373781a3e6e4da269f3
James Bogosian <bogosj@users.noreply.github.com>
2022-12-02 13:57:03 +0000

    Day 2 complete.

--- 2022/day02/main.go
+++ 2022/day02/main.go
@@ -29,7 +29,27 @@ func part1(in []string) {
 	fmt.Printf("Total score: %d\n", total)
 }
 
+// Rock 1 A Paper 2 B Scissors 3 C
+// X lose, Y draw, Z win
+// Outcome 0 loss, 3 tie, 6 win
+
 func part2(in []string) {
+	scores := map[string]int{
+		"A X": 3,
+		"A Y": 4,
+		"A Z": 8,
+		"B X": 1,
+		"B Y": 5,
+		"B Z": 9,
+		"C X": 2,
+		"C Y": 6,
+		"C Z": 7,
+	}
+	total := 0
+	for _, line := range in {
+		total += scores[line]
+	}
+	fmt.Printf("Total score: %d\n", total)
 }
 
 func main() {
