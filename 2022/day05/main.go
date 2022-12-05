d23d5efc6812a3b90e54ee0538bcd59c50272a3a
James Bogosian <bogosj@users.noreply.github.com>
2022-12-05 19:28:18 +0000

    Day 5 part 1 done.

--- 2022/day05/main.go
+++ 2022/day05/main.go
@@ -2,15 +2,28 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/bogosj/advent-of-code/fileinput"
+	"github.com/bogosj/advent-of-code/intmath"
 )
 
-func part1(in []string) {
+func part1(in map[int][]string) {
+	for _, move := range moves() {
+		for i := 0; i < move[0]; i++ {
+			crate := in[move[1]][0]
+			in[move[2]] = append([]string{crate}, in[move[2]]...)
+			in[move[1]] = in[move[1]][1:]
+		}
+	}
+	for i := 1; i <= 9; i++ {
+		fmt.Print(in[i][0])
+	}
+	fmt.Println()
 }
 
-func part2(in []string) {
+func part2(in map[int][]string) {
 }
 
 func main() {
@@ -24,6 +37,31 @@ func main() {
 	fmt.Println("Part 2 done in", time.Since(start))
 }
 
-func input() []string {
-	return fileinput.ReadLines("input.txt")
+func input() map[int][]string {
+	return map[int][]string{
+		1: {"R", "C", "H"},
+		2: {"F", "S", "L", "H", "J", "B"},
+		3: {"Q", "T", "J", "H", "D", "M", "R"},
+		4: {"J", "B", "Z", "H", "R", "G", "S"},
+		5: {"B", "C", "D", "T", "Z", "F", "P", "R"},
+		6: {"G", "C", "H", "T"},
+		7: {"L", "W", "P", "B", "Z", "V", "N", "S"},
+		8: {"C", "G", "Q", "J", "R"},
+		9: {"S", "F", "P", "H", "R", "T", "D", "L"},
+	}
+}
+
+func moves() [][]int {
+	ret := [][]int{}
+	for _, line := range fileinput.ReadLines("input.txt") {
+		if strings.Contains(line, "move") {
+			subs := strings.Split(line, " ")
+			ret = append(ret, []int{
+				intmath.Atoi(subs[1]),
+				intmath.Atoi(subs[3]),
+				intmath.Atoi(subs[5]),
+			})
+		}
+	}
+	return ret
 }
