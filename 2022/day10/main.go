47a5fb342c1dde9bde7800ca7db1b384441d284e
James Bogosian <bogosj@users.noreply.github.com>
2022-12-10 14:23:19 +0000

    Day 10 part 1 done.

--- 2022/day10/main.go
+++ 2022/day10/main.go
@@ -2,12 +2,39 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/bogosj/advent-of-code/fileinput"
+	"github.com/bogosj/advent-of-code/intmath"
 )
 
+func evalStrength(cycle, register int) int {
+	switch cycle {
+	case 20, 60, 100, 140, 180, 220:
+		return register * cycle
+	}
+	return 0
+}
+
 func part1(in []string) {
+	cycle := 1
+	register := 1
+	strength := 0
+	for _, line := range in {
+		inst := strings.Split(line, " ")[0]
+		if inst == "noop" {
+			strength += evalStrength(cycle, register)
+			cycle++
+		} else {
+			strength += evalStrength(cycle, register)
+			cycle++
+			strength += evalStrength(cycle, register)
+			cycle++
+			register += intmath.Atoi(strings.Split(line, " ")[1])
+		}
+	}
+	fmt.Printf("Signal strength: %d\n", strength)
 }
 
 func part2(in []string) {
