22fcfb3836385b572283cbd38af97a4ea6b7d4c0
James Bogosian <bogosj@users.noreply.github.com>
2022-12-12 16:38:22 +0000

    Day 12 part 1 done.

--- 2022/day12/main.go
+++ 2022/day12/main.go
@@ -5,12 +5,56 @@ import (
 	"time"
 
 	"github.com/bogosj/advent-of-code/fileinput"
+	"github.com/bogosj/advent-of-code/intmath"
 )
 
-func part1(in []string) {
+type elevationMap struct {
+	e          map[intmath.Point]int
+	start, end intmath.Point
 }
 
-func part2(in []string) {
+type state struct {
+	p     intmath.Point
+	steps int
+}
+
+func (s state) nextStates(e elevationMap) (ret []state) {
+	for _, n := range s.p.Neighbors() {
+		delta := e.e[n] - e.e[s.p]
+		if delta <= 1 {
+			_, ok := e.e[n]
+			if ok {
+				ret = append(ret, state{p: n, steps: s.steps + 1})
+			}
+		}
+	}
+	return
+}
+
+func part1(in elevationMap) {
+	states := []state{{p: in.start}}
+	visited := map[intmath.Point]bool{}
+	steps := 0
+	count := 0
+	for len(states) > 0 {
+		state := states[0]
+		states = states[1:]
+		if visited[state.p] {
+			continue
+		}
+		visited[state.p] = true
+		if state.p == in.end {
+			steps = state.steps
+			break
+		}
+		ns := state.nextStates(in)
+		states = append(states, ns...)
+		count++
+	}
+	fmt.Printf("It takes %d steps to reach the end.\n", steps)
+}
+
+func part2(in elevationMap) {
 }
 
 func main() {
@@ -24,6 +68,21 @@ func main() {
 	fmt.Println("Part 2 done in", time.Since(start))
 }
 
-func input() []string {
-	return fileinput.ReadLines("input.txt")
+func input() elevationMap {
+	e := elevationMap{}
+	e.e = map[intmath.Point]int{}
+	for y, line := range fileinput.ReadLines("input.txt") {
+		for x, c := range line {
+			curr := intmath.Point{X: x, Y: y}
+			e.e[curr] = int(c - 'a')
+			if c == 'S' {
+				e.e[curr] = 0
+				e.start = curr
+			} else if c == 'E' {
+				e.e[curr] = 25
+				e.end = curr
+			}
+		}
+	}
+	return e
 }
