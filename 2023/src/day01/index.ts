2892c3c3d5ec4d5fbc2fa843caeacdc508f0efd8
James Bogosian <bogosj@gmail.com>
2023-12-01 15:21:14 +0000

    Day 1 part 1 done.

--- 2023/src/day01/index.ts
+++ 2023/src/day01/index.ts
@@ -2,10 +2,23 @@ import run from "aocrunner";
 
 const parseInput = (rawInput: string) => rawInput;
 
+const isCharNumber = (c: string) => {
+  return c >= '0' && c <= '9';
+};
+
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput);
-
-  return;
+  const lines = input.split('\n');
+  const digitsOnly = lines.map((line) => {
+    return line.split('').filter((c) => isCharNumber(c));
+  });
+  const values = digitsOnly.map((digits) => {
+    let val = parseInt(digits[0], 10) * 10;
+    val += parseInt(digits.pop(), 10);
+    return val;
+  });
+  console.log(values);
+  return values.reduce((total, val) => total + val);
 };
 
 const part2 = (rawInput: string) => {
@@ -17,10 +30,13 @@ const part2 = (rawInput: string) => {
 run({
   part1: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `1abc2
+         pqr3stu8vwx
+         a1b2c3d4e5f
+         treb7uchet`,
+        expected: 142,
+      },
     ],
     solution: part1,
   },
