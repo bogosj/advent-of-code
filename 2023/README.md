18d9cff45cf145518ac919f1a29f5f08059bcbdc
James Bogosian <bogosj@gmail.com>
2023-12-02 15:52:42 +0000

    Day 2 part 2 solved.

--- 2023/README.md
+++ 2023/README.md
@@ -12,7 +12,7 @@
 <!--SOLUTIONS-->
 
 [![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%85/green)](src/day01)
-[![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%86/yellow)](src/day02)
+[![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%85/green)](src/day02)
 ![Day](https://badgen.net/badge/03/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/04/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/05/%E2%98%86%E2%98%86/gray)
@@ -76,9 +76,9 @@ Both parts: 26.985ms
 
 ```
 Day 02
-Time part 1: 4.301ms
-Time part 2: -
-Both parts: 4.301ms
+Time part 1: 31.86ms
+Time part 2: 1.897ms
+Both parts: 33.757ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 3/50
-Total time: 31.286ms
+Total stars: 4/50
+Total time: 60.742ms
 ```
 
 <!--/RESULTS-->
--- 2023/src/day02/index.ts
+++ 2023/src/day02/index.ts
@@ -34,14 +34,25 @@ const part1 = (rawInput: string) => {
     })) {
       result += parseInt(game, 10);
     }
-  })
+  });
   return result; 
 };
 
 const part2 = (rawInput: string) => {
   const input = parseInput(rawInput);
-
-  return;
+  let result = 0;
+  Object.entries(input).forEach(([game, results]) => {
+    const reds = [];
+    const blues = [];
+    const greens = [];
+    results.forEach(result => {
+      reds.push(result.red || 0);
+      blues.push(result.blue || 0);
+      greens.push(result.green || 0);
+    });
+    result += Math.max(...reds) * Math.max(...blues) * Math.max(...greens);
+  });
+  return result;
 };
 
 run({
@@ -62,10 +73,16 @@ run({
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `
+        Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+        Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+        Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+        Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+        Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+        `,
+        expected: 2286,
+      },
     ],
     solution: part2,
   },
