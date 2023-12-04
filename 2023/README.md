c3309a3b94e178b5e7dc5293577f3d7fa569fd82
James Bogosian <bogosj@gmail.com>
2023-12-04 01:06:11 +0000

    Day 3 part 2 solved.

--- 2023/README.md
+++ 2023/README.md
@@ -13,7 +13,7 @@
 
 [![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%85/green)](src/day01)
 [![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%85/green)](src/day02)
-[![Day](https://badgen.net/badge/03/%E2%98%85%E2%98%86/yellow)](src/day03)
+[![Day](https://badgen.net/badge/03/%E2%98%85%E2%98%85/green)](src/day03)
 ![Day](https://badgen.net/badge/04/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/05/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/06/%E2%98%86%E2%98%86/gray)
@@ -83,9 +83,9 @@ Both parts: 2.566ms
 
 ```
 Day 03
-Time part 1: 4.966ms
-Time part 2: -
-Both parts: 4.966ms
+Time part 1: 7.487ms
+Time part 2: 7.224ms
+Both parts: 14.71ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 5/50
-Total time: 38.762ms
+Total stars: 6/50
+Total time: 48.506ms
 ```
 
 <!--/RESULTS-->
--- 2023/src/day03/index.ts
+++ 2023/src/day03/index.ts
@@ -43,17 +43,59 @@ const part1 = (rawInput: string) => {
           goodNumbers.push(accumulator);
         }
         accumulator = 0;
-        isGoodNumber = false; 
+        isGoodNumber = false;
       }
     }
   }
   return goodNumbers.reduce((prev, curr) => prev + curr);
 };
 
+const touchesStar = (input: string[][], y: number, x: number): { x: number, y: number } => {
+  for (let y2 = y - 1; y2 <= y + 1; y2++) {
+    for (let x2 = x - 1; x2 <= x + 1; x2++) {
+      try {
+        if (input[y2][x2] == '*') {
+          return { x: x2, y: y2 };
+        }
+      } catch (error) { }
+    }
+  }
+  return undefined;
+};
+
 const part2 = (rawInput: string) => {
   const input = parseInput(rawInput);
 
-  return;
+  const gears = {};
+
+  let accumulator = 0;
+  let starPosition = undefined;
+  for (let y = 0; y < input.length; y++) {
+    for (let x = 0; x < input[y].length; x++) {
+      if (isNumber(input[y][x])) {
+        accumulator = accumulator * 10 + parseInt(input[y][x], 10);
+        starPosition = starPosition || touchesStar(input, y, x);
+      } else {
+        if (starPosition) {
+          const key = `${starPosition.x}|${starPosition.y}`;
+          if (!gears[key]) {
+            gears[key] = [];
+          }
+          gears[key].push(accumulator);
+        }
+        accumulator = 0; 
+        starPosition = undefined;
+      }
+    }
+  }
+
+  let retVal = 0;
+  Object.values(gears).forEach(parts => {
+    if (parts.length == 2) {
+      retVal += parts[0] * parts[1];
+    }
+  });
+  return retVal;
 };
 
 run({
@@ -98,5 +140,5 @@ run({
     solution: part2,
   },
   trimTestInputs: true,
-  onlyTests: true,
+  onlyTests: false,
 });
