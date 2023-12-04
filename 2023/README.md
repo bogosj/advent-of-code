a7abc671fd1aafe16351b5e060edadceabac6ebf
James Bogosian <bogosj@gmail.com>
2023-12-04 16:06:38 +0000

    Day 4 part 2 solved.

--- 2023/README.md
+++ 2023/README.md
@@ -14,7 +14,7 @@
 [![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%85/green)](src/day01)
 [![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%85/green)](src/day02)
 [![Day](https://badgen.net/badge/03/%E2%98%85%E2%98%85/green)](src/day03)
-[![Day](https://badgen.net/badge/04/%E2%98%85%E2%98%86/yellow)](src/day04)
+[![Day](https://badgen.net/badge/04/%E2%98%85%E2%98%85/green)](src/day04)
 ![Day](https://badgen.net/badge/05/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/06/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/07/%E2%98%86%E2%98%86/gray)
@@ -90,9 +90,9 @@ Both parts: 14.71ms
 
 ```
 Day 04
-Time part 1: 5.171ms
-Time part 2: -
-Both parts: 5.171ms
+Time part 1: 3.02ms
+Time part 2: 9.272ms
+Both parts: 12.292ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 7/50
-Total time: 53.677ms
+Total stars: 8/50
+Total time: 60.798ms
 ```
 
 <!--/RESULTS-->
--- 2023/src/day04/index.ts
+++ 2023/src/day04/index.ts
@@ -26,10 +26,10 @@ const part1 = (rawInput: string) => {
     let cardScore = 0;
     winningNumbers.map(num => {
       if (myNumbers.includes(num)) {
-        if (cardScore ==0) { 
+        if (cardScore == 0) {
           cardScore = 1;
         } else {
-          cardScore *=2;
+          cardScore *= 2;
         }
       }
     });
@@ -40,8 +40,36 @@ const part1 = (rawInput: string) => {
 
 const part2 = (rawInput: string) => {
   const input = parseInput(rawInput);
-
-  return;
+  let cardScores = [];
+  input.map(card => {
+    const winningNumbers = [];
+    const myNumbers = [];
+    card = card.slice(2);
+    let next = card.shift();
+    while (next != '|') {
+      winningNumbers.push(parseInt(next, 10));
+      next = card.shift();
+    }
+    next = card.shift();
+    while (next) {
+      myNumbers.push(parseInt(next, 10));
+      next = card.shift();
+    }
+    let cardScore = 0;
+    winningNumbers.map(num => {
+      if (myNumbers.includes(num)) {
+        cardScore += 1;
+      }
+    });
+    cardScores.push(cardScore);
+  });
+  const cardCounts = Array(cardScores.length).fill(1);
+  for (let i = 0; i < cardScores.length; i++) {
+    for (let j = 0; j < cardScores[i]; j++) {
+      cardCounts[i + j + 1] += cardCounts[i];
+    } 
+  } 
+  return cardCounts.reduce((p, c) => p + c);
 };
 
 run({
@@ -63,10 +91,17 @@ run({
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `
+        Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+        Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+        Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+        Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+        Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+        Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+        `,
+        expected: 30,
+      },
     ],
     solution: part2,
   },
