cfd35891d123838ee89ad5eaf0a0e768214380a1
James Bogosian <bogosj@gmail.com>
2023-12-12 00:24:34 +0000

    Day 11 part 2 solved.

--- 2023/README.md
+++ 2023/README.md
@@ -21,7 +21,7 @@
 [![Day](https://badgen.net/badge/08/%E2%98%85%E2%98%85/green)](src/day08)
 [![Day](https://badgen.net/badge/09/%E2%98%85%E2%98%85/green)](src/day09)
 [![Day](https://badgen.net/badge/10/%E2%98%85%E2%98%86/yellow)](src/day10)
-[![Day](https://badgen.net/badge/11/%E2%98%85%E2%98%86/yellow)](src/day11)
+[![Day](https://badgen.net/badge/11/%E2%98%85%E2%98%85/green)](src/day11)
 ![Day](https://badgen.net/badge/12/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/13/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/14/%E2%98%86%E2%98%86/gray)
@@ -139,9 +139,9 @@ Both parts: 534.426ms
 
 ```
 Day 11
-Time part 1: 11.628ms
-Time part 2: -
-Both parts: 11.628ms
+Time part 1: 9.563ms
+Time part 2: 14.455ms
+Both parts: 24.018ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 20/50
-Total time: 3776.421ms
+Total stars: 21/50
+Total time: 3788.812ms
 ```
 
 <!--/RESULTS-->
--- 2023/src/day11/index.ts
+++ 2023/src/day11/index.ts
@@ -4,62 +4,63 @@ const parseInput = (rawInput: string) => {
   return rawInput.split('\n').map(line => line.split(''));
 };
 
-const expandUniverse = (universe: Array<Array<string>>) => {
-  const expandedY: Array<Array<string>> = [];
-  universe.forEach(line => {
+const expandUniverse = (galaxyCoords: Array<Array<number>>, universe: Array<Array<string>>, expansionRate: number) => {
+  const linesToExpand = [];
+  universe.forEach((line, y) => {
     if (line.every(char => char == '.')) {
-      expandedY.push(line);
+      linesToExpand.push(y)
     }
-    expandedY.push(line);
   });
 
   const columnsToExpand = [];
-  for (let x = 0; x < expandedY[0].length; x++) {
-    if (expandedY.map(line => line[x]).every(char => char == '.')) {
+  for (let x = 0; x < universe[0].length; x++) {
+    if (universe.map(line => line[x]).every(char => char == '.')) {
       columnsToExpand.push(x);
     }
   }
-  const expandedX = [];
-  expandedY.forEach(line => {
-    const newLine: Array<string> = [];
-    line.forEach((char, idx) => {
-      if (columnsToExpand.includes(idx)) {
-        newLine.push('.');
-      }
-      newLine.push(char);
-    })
-    expandedX.push(newLine);
-  });
-  return expandedX;
+  const newCoords = [];
+  galaxyCoords.forEach(coord => {
+    const colsBefore = columnsToExpand.filter(x => x < coord[1]);
+    const linesAbove = linesToExpand.filter(y => y < coord[0]);
+    newCoords.push(
+      [
+        coord[0] + (linesAbove.length * (expansionRate - 1)),
+        coord[1] + (colsBefore.length * (expansionRate - 1)),
+      ]);
+  })
+  return newCoords;
 };
 
 const manhattanDistance = (a: Array<number>, b: Array<number>): number => {
   return Math.abs(a[0] - b[0]) + Math.abs(a[1] - b[1]);
 };
 
-const part1 = (rawInput: string) => {
+const solver = (rawInput: string, expansionRate: number): number => {
   const input = parseInput(rawInput);
   const galaxyCoords = [];
-  expandUniverse(input).forEach((line, y) => {
+  input.forEach((line, y) => {
     return line.forEach((char, x) => {
       if (char == '#') {
         galaxyCoords.push([y, x]);
       }
     });
   });
+  const expandedGalaxyCoords = expandUniverse(galaxyCoords, input, expansionRate);
   const distances = [];
-  for (let i = 0; i < galaxyCoords.length; i++) {
-    for (let j = i + 1; j < galaxyCoords.length; j++) {
-      distances.push(manhattanDistance(galaxyCoords[i], galaxyCoords[j]));
+  for (let i = 0; i < expandedGalaxyCoords.length; i++) {
+    for (let j = i + 1; j < expandedGalaxyCoords.length; j++) {
+      distances.push(manhattanDistance(expandedGalaxyCoords[i], expandedGalaxyCoords[j]));
     }
   }
   return distances.reduce((a, b) => a + b);
+}
+
+const part1 = (rawInput: string) => {
+  return solver(rawInput, 2);
 };
 
 const part2 = (rawInput: string) => {
-  const input = parseInput(rawInput);
-
-  return;
+  return solver(rawInput, 1_000_000);
 };
 
 run({
