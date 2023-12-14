b6fb0553ddfe960d4c08920b18e618e3a1500067
James Bogosian <bogosj@gmail.com>
2023-12-14 16:46:33 -0500

    Day 14 part 1 solved.

--- 2023/README.md
+++ 2023/README.md
@@ -24,7 +24,7 @@
 [![Day](https://badgen.net/badge/11/%E2%98%85%E2%98%85/green)](src/day11)
 [![Day](https://badgen.net/badge/12/%E2%98%85%E2%98%85/green)](src/day12)
 [![Day](https://badgen.net/badge/13/%E2%98%85%E2%98%85/green)](src/day13)
-![Day](https://badgen.net/badge/14/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/14/%E2%98%85%E2%98%86/yellow)](src/day14)
 ![Day](https://badgen.net/badge/15/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/16/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/17/%E2%98%86%E2%98%86/gray)
@@ -153,16 +153,16 @@ Both parts: 2211.995ms
 
 ```
 Day 13
-Time part 1: 2.573ms
-Time part 2: 0.73ms
-Both parts: 3.304ms
+Time part 1: 2.587ms
+Time part 2: 0.749ms
+Both parts: 3.337ms
 ```
 
 ```
 Day 14
-Time part 1: -
+Time part 1: 6.826ms
 Time part 2: -
-Both parts: -
+Both parts: 6.826ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 25/50
-Total time: 6008.581ms
+Total stars: 26/50
+Total time: 6015.439ms
 ```
 
 <!--/RESULTS-->
--- 2023/src/day14/index.ts
+++ 2023/src/day14/index.ts
@@ -1,11 +1,71 @@
 import run from "aocrunner";
 
-const parseInput = (rawInput: string) => rawInput;
+const parseInput = (rawInput: string): Array<Array<string>> => {
+  return rawInput.split('\n').map(line => line.split(''));
+};
+
+const directionShifts = {
+  'N': [-1, 0],
+  'S': [1, 0],
+  'W': [0, -1],
+  'E': [0, 1],
+};
+
+const newPositionIsValid = (rocks: Array<Array<string>>, position: Array<number>): boolean => {
+  return (
+    position[0] >= 0 &&
+    position[1] >= 0 &&
+    position[0] < rocks.length &&
+    position[1] < rocks[0].length
+  )
+}
+
+const shiftRocks = (rocks: Array<Array<string>>, direction: string) => {
+  let moves = 0;
+  const shift = directionShifts[direction];
+
+  while (true) {
+    for (let y = 0; y < rocks.length; y++) {
+      for (let x = 0; x < rocks[0].length; x++) {
+        const curr = rocks[y][x];
+        if (curr == 'O') {
+          const newPosition = [y + shift[0], x + shift[1]];
+          if (newPositionIsValid(rocks, newPosition)) {
+            if (rocks[newPosition[0]][newPosition[1]] == '.') {
+              rocks[y][x] = '.';
+              rocks[newPosition[0]][newPosition[1]] = 'O';
+              moves++;
+            }
+          }
+        }
+      }
+    }
+    if (moves == 0) {
+      break;
+    }
+    moves = 0;
+  }
+}
+
+const calculateLoad = (rocks: Array<Array<string>>): number => {
+  const height = rocks.length;
+  return rocks.map((line, idx) => {
+    return line.map(c => {
+      if (c != 'O') {
+        return 0;
+      }
+      return height - idx;
+    }).reduce((a, b) => a + b)
+  }).reduce((a, b) => a + b);
+}
+
 
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput);
 
-  return;
+  shiftRocks(input, 'N');
+
+  return calculateLoad(input);
 };
 
 const part2 = (rawInput: string) => {
@@ -17,10 +77,21 @@ const part2 = (rawInput: string) => {
 run({
   part1: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `
+        O....#....
+        O.OO#....#
+        .....##...
+        OO.#O....O
+        .O.....O#.
+        O.#..O.#.#
+        ..O..#O..O
+        .......O..
+        #....###..
+        #OO..#....
+        `,
+        expected: 136,
+      },
     ],
     solution: part1,
   },
