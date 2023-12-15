2d73e0e31c24a2a0d5808ed7bfe5bc2f731e6a64
James Bogosian <bogosj@gmail.com>
2023-12-15 10:28:22 -0500

    Day 15 part 2 solved.

--- 2023/src/day15/index.ts
+++ 2023/src/day15/index.ts
@@ -20,10 +20,55 @@ const part1 = (rawInput: string) => {
   return input.map(hashString).reduce((a, b) => a + b);
 };
 
+interface lense {
+  label: string,
+  focalLength: number
+}
+
 const part2 = (rawInput: string) => {
   const input = parseInput(rawInput);
+  const boxes: { [key: number]: Array<lense> } = {};
+  for (let i = 0; i < 256; i++) {
+    boxes[i] = [];
+  }
+  input.forEach(v => {
+    if (v.includes('=')) {
+      const [label, focalLength] = v.split('=');
+      const hash = hashString(label);
+      const lense = { label: label, focalLength: parseInt(focalLength, 10) }
+      let replaced = false;
+      boxes[hash].map(l => {
+        if (l.label == lense.label) {
+          l.focalLength = lense.focalLength;
+          replaced = true;
+          return lense;
+        }
+        return l;
+      });
+      if (!replaced) {
+        boxes[hash].push(lense);
+      }
+    } else {
+      const [label, focalLength] = v.split('-');
+      const hash = hashString(label);
+      const lense = { label: label, focalLength: parseInt(focalLength, 10) };
+      boxes[hash] = boxes[hash].filter(l => {
+        if (l.label != lense.label) {
+          return l;
+        }
+      });
+    }
+  });
 
-  return;
+  return Object.entries(boxes).map(([k, v]) => {
+    const mag = parseInt(k, 10) + 1;
+    if (v.length == 0) {
+      return 0;
+    }
+    return v.map((lense, idx) => {
+      return mag * (idx + 1) * lense.focalLength;
+    }).reduce((a, b) => a + b);
+  }).reduce((a, b) => a + b);
 };
 
 run({
@@ -38,10 +83,10 @@ run({
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7`,
+        expected: 145,
+      },
     ],
     solution: part2,
   },
