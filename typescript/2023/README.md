f5f222b7ff2ec92c15532bf4e7bc23cc8a6cb7d8
James Bogosian <bogosj@gmail.com>
2023-12-18 18:15:14 -0500

    Day 17 done.

--- typescript/2023/README.md
+++ typescript/2023/README.md
@@ -27,7 +27,7 @@
 [![Day](https://badgen.net/badge/14/%E2%98%85%E2%98%85/green)](src/day14)
 [![Day](https://badgen.net/badge/15/%E2%98%85%E2%98%85/green)](src/day15)
 [![Day](https://badgen.net/badge/16/%E2%98%85%E2%98%85/green)](src/day16)
-[![Day](https://badgen.net/badge/17/%E2%98%85%E2%98%86/yellow)](src/day17)
+[![Day](https://badgen.net/badge/17/%E2%98%85%E2%98%85/green)](src/day17)
 ![Day](https://badgen.net/badge/18/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/19/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/20/%E2%98%86%E2%98%86/gray)
@@ -181,9 +181,9 @@ Both parts: 67122.832ms
 
 ```
 Day 17
-Time part 1: 352.892ms
-Time part 2: -
-Both parts: 352.892ms
+Time part 1: 368.323ms
+Time part 2: 1251.785ms
+Both parts: 1620.109ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 33/50
-Total time: 74693.164ms
+Total stars: 34/50
+Total time: 75960.38ms
 ```
 
 <!--/RESULTS-->
--- typescript/2023/src/day17/index.ts
+++ typescript/2023/src/day17/index.ts
@@ -52,8 +52,7 @@ const atGoal = (state: WalkState, city: Array<Array<number>>): boolean => {
 
 const visitedKey = (s: WalkState): string => { return `${s.y},${s.x},${s.dir},${s.stepsInDir}`; }
 
-const shortestPath = (city: Array<Array<number>>): number => {
-  let lowestHeat = Number.MAX_VALUE;
+const shortestPath = (city: Array<Array<number>>, minBeforeTurn: number, maxBeforeTurn: number): number => {
   const visited = {};
   const statesQueue = new MinPriorityQueue<WalkState>(getHeatLoss);
   statesQueue.enqueue({ x: 1, y: 0, heatLoss: 0, dir: 'R', stepsInDir: 1 });
@@ -66,11 +65,6 @@ const shortestPath = (city: Array<Array<number>>): number => {
       continue
     }
 
-    // If we took 4 steps in this direction, it's invalid.
-    if (next.stepsInDir > 3) {
-      continue; 
-    }
-
     // Have we been here before, entering from this direciton?
     // If so, check if the prior way was better.
     const last = visited[visitedKey(next)];
@@ -81,6 +75,9 @@ const shortestPath = (city: Array<Array<number>>): number => {
 
     next.heatLoss += city[next.y][next.x];
     if (atGoal(next, city)) {
+      if (next.stepsInDir < minBeforeTurn) {
+        continue;
+      }
       return next.heatLoss;
     }
 
@@ -92,26 +89,34 @@ const shortestPath = (city: Array<Array<number>>): number => {
         dir: dir,
         stepsInDir: next.stepsInDir + 1
       }
-      if (dir != next.dir) {
+      if (dir != next.dir) { // Turning
+        if (next.stepsInDir < minBeforeTurn) {
+          return;
+        }
         newNext.stepsInDir = 1;
+      } else { // Going straight
+        // Too too many forward steps
+        if (newNext.stepsInDir > maxBeforeTurn) {
+          return;
+        }
       }
       statesQueue.enqueue(newNext);
     });
   }
 
-  return lowestHeat;
+  return Number.MAX_VALUE;
 }
 
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput);
 
-  return shortestPath(input);
+  return shortestPath(input, 1, 3);
 };
 
 const part2 = (rawInput: string) => {
   const input = parseInput(rawInput);
 
-  return;
+  return shortestPath(input, 4, 10);
 };
 
 run({
@@ -140,10 +145,34 @@ run({
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `
+        2413432311323
+        3215453535623
+        3255245654254
+        3446585845452
+        4546657867536
+        1438598798454
+        4457876987766
+        3637877979653
+        4654967986887
+        4564679986453
+        1224686865563
+        2546548887735
+        4322674655533
+        `,
+        expected: 94,
+      },
+      {
+        input: `
+        111111111111
+        999999999991
+        999999999991
+        999999999991
+        999999999991
+        `,
+        expected: 71,
+      },
     ],
     solution: part2,
   },
