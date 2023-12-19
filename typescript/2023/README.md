8d303c177c077fbcc2362a4a94a4ec37e01499d2
James Bogosian <bogosj@gmail.com>
2023-12-19 10:32:27 -0500

    Day 18 part 1 solved.

--- typescript/2023/README.md
+++ typescript/2023/README.md
@@ -28,7 +28,7 @@
 [![Day](https://badgen.net/badge/15/%E2%98%85%E2%98%85/green)](src/day15)
 [![Day](https://badgen.net/badge/16/%E2%98%85%E2%98%85/green)](src/day16)
 [![Day](https://badgen.net/badge/17/%E2%98%85%E2%98%85/green)](src/day17)
-![Day](https://badgen.net/badge/18/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/18/%E2%98%85%E2%98%86/yellow)](src/day18)
 ![Day](https://badgen.net/badge/19/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/20/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/21/%E2%98%86%E2%98%86/gray)
@@ -188,9 +188,9 @@ Both parts: 1620.109ms
 
 ```
 Day 18
-Time part 1: -
+Time part 1: 0.383ms
 Time part 2: -
-Both parts: -
+Both parts: 0.383ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 34/50
-Total time: 75960.38ms
+Total stars: 35/50
+Total time: 75960.763ms
 ```
 
 <!--/RESULTS-->
--- typescript/2023/src/day18/index.ts
+++ typescript/2023/src/day18/index.ts
@@ -1,11 +1,63 @@
 import run from "aocrunner";
 
-const parseInput = (rawInput: string) => rawInput;
+interface Instruction {
+  dir: string,
+  len: number,
+  color: string,
+}
+
+const moves = {
+  'U': { y: -1, x: 0 },
+  'D': { y: 1, x: 0 },
+  'L': { x: -1, y: 0 },
+  'R': { x: 1, y: 0 },
+}
+
+const parseInput = (rawInput: string): Instruction[] => {
+  return rawInput.split('\n').map(line => {
+    const tokens = line.split(' ');
+    return {
+      dir: tokens[0],
+      len: parseInt(tokens[1], 10),
+      color: tokens[2].substring(2, tokens[2].length - 1)
+    }
+  });
+};
+
+interface Point {
+  x: number,
+  y: number,
+}
+
+const getArea = (verticies: Point[]): number => {
+  let area = 0;
+
+  let j = verticies.length - 1;
+  for (let i = 0; i < verticies.length; i++) {
+    area += (verticies[j].x + verticies[i].x) * (verticies[j].y - verticies[i].y);
+    j = i;
+  }
+
+  return Math.abs(area) / 2;
+};
+
+const trenchArea = (instructions: Instruction[]): number => {
+  const pos = { x: 0, y: 0 };
+  const verticies = [];
+  let perimeter = 0;
+  instructions.forEach((instruction) => {
+    pos.x += moves[instruction.dir].x * instruction.len;
+    pos.y += moves[instruction.dir].y * instruction.len;
+    perimeter += instruction.len;
+    verticies.push({ x: pos.x, y: pos.y });
+  });
+
+  return getArea(verticies) + perimeter / 2 + 1;
+};
 
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput);
-
-  return;
+  return trenchArea(input);
 };
 
 const part2 = (rawInput: string) => {
@@ -17,10 +69,25 @@ const part2 = (rawInput: string) => {
 run({
   part1: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `
+        R 6 (#70c710)
+        D 5 (#0dc571)
+        L 2 (#5713f0)
+        D 2 (#d2c081)
+        R 2 (#59c680)
+        D 2 (#411b91)
+        L 5 (#8ceee2)
+        U 2 (#caa173)
+        L 1 (#1b58a2)
+        U 2 (#caa171)
+        R 2 (#7807d2)
+        U 3 (#a77fa3)
+        L 2 (#015232)
+        U 2 (#7a21e3)
+        `,
+        expected: 62,
+      },
     ],
     solution: part1,
   },
@@ -36,3 +103,4 @@ run({
   trimTestInputs: true,
   onlyTests: false,
 });
+
