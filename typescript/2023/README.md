cf783ec05eac5e1274892b814094ba4d974795fe
James Bogosian <bogosj@gmail.com>
2023-12-20 15:10:14 -0500

    Day 19 part 2 solved.

--- typescript/2023/README.md
+++ typescript/2023/README.md
@@ -29,7 +29,7 @@
 [![Day](https://badgen.net/badge/16/%E2%98%85%E2%98%85/green)](src/day16)
 [![Day](https://badgen.net/badge/17/%E2%98%85%E2%98%85/green)](src/day17)
 [![Day](https://badgen.net/badge/18/%E2%98%85%E2%98%85/green)](src/day18)
-[![Day](https://badgen.net/badge/19/%E2%98%85%E2%98%86/yellow)](src/day19)
+[![Day](https://badgen.net/badge/19/%E2%98%85%E2%98%85/green)](src/day19)
 ![Day](https://badgen.net/badge/20/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/21/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/22/%E2%98%86%E2%98%86/gray)
@@ -195,9 +195,9 @@ Both parts: 1.003ms
 
 ```
 Day 19
-Time part 1: 32.397ms
-Time part 2: -
-Both parts: 32.397ms
+Time part 1: 30.639ms
+Time part 2: 8.052ms
+Both parts: 38.691ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 37/50
-Total time: 75993.781ms
+Total stars: 38/50
+Total time: 76000.075ms
 ```
 
 <!--/RESULTS-->
--- typescript/2023/src/day19/index.ts
+++ typescript/2023/src/day19/index.ts
@@ -1,5 +1,7 @@
 import run from "aocrunner";
 
+const sum = (arr: number[]) => arr.reduce((a, b) => a + b);
+
 interface Part {
   x: number,
   m: number,
@@ -63,10 +65,74 @@ const part1 = (rawInput: string) => {
   return parts.filter(partFilter).map(valuePart).reduce((a, b) => a + b);
 };
 
-const part2 = (rawInput: string) => {
-  const input = parseInput(rawInput);
+const valueOfFilters = (filters: string[]): number => {
+  const ranges = {
+    x: [1, 4000],
+    m: [1, 4000],
+    a: [1, 4000],
+    s: [1, 4000],
+  }
+  filters.forEach(filter => {
+    const letter = filter.substring(0, 1);
+    const gt = filter.substring(1, 2) == '>';
+    const val = parseInt(filter.substring(2), 10);
+    if (gt) {
+      if (val > ranges[letter][0]) {
+        ranges[letter][0] = val + 1;
+      }
+    } else {
+      if (val < ranges[letter][1]) {
+        ranges[letter][1] = val - 1;
+      }
+    }
+  });
 
-  return;
+  return (
+    (ranges.x[1] - ranges.x[0] + 1) *
+    (ranges.m[1] - ranges.m[0] + 1) *
+    (ranges.a[1] - ranges.a[0] + 1) *
+    (ranges.s[1] - ranges.s[0] + 1)
+  )
+};
+
+const invertRule = (rule: string): string => {
+  const gt = rule.charAt(1) == '>';
+  const comp = gt ? '<' : '>';
+  let val = parseInt(rule.substring(2), 10);
+  val = gt ? val + 1 : val - 1;
+  return `${rule.charAt(0)}${comp}${val}`;
+};
+
+const allValidRules = (rules: string[], rulesToApply: string[], allRules): string[][] => {
+  const ruleToEval = rules[0];
+  const futureRules = rules.slice(1);
+  if (ruleToEval.includes(':')) {
+    const [rule, nextRuleName] = ruleToEval.split(':');
+    return [  
+      allValidRules([...allRules[nextRuleName]], [...rulesToApply, rule], allRules),
+      allValidRules(futureRules, [...rulesToApply, invertRule(rule)], allRules)
+    ].flat(); 
+  } else { // default rule
+    if (ruleToEval == 'R') {
+      return [];
+    }
+    if (ruleToEval == 'A') {
+      return [
+        [...rulesToApply]
+      ];
+    } 
+    return [
+      allValidRules([...allRules[ruleToEval]], [...rulesToApply], allRules),
+    ].flat();
+  }
+};
+
+const part2 = (rawInput: string) => {
+  const [rules] = parseInput(rawInput);
+  rules['A'] = ['A']
+  rules['R'] = ['R']
+  const allRules = allValidRules(rules['in'], [], rules);
+  return sum(allRules.map(valueOfFilters));
 };
 
 run({
@@ -99,10 +165,28 @@ run({
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `
+        px{a<2006:qkq,m>2090:A,rfg}
+        pv{a>1716:R,A}
+        lnx{m>1548:A,A}
+        rfg{s<537:gd,x>2440:R,A}
+        qs{s>3448:A,lnx}
+        qkq{x<1416:A,crn}
+        crn{x>2662:A,R}
+        in{s<1351:px,qqz}
+        qqz{s>2770:qs,m<1801:hdj,R}
+        gd{a>3333:R,R}
+        hdj{m>838:A,pv}
+        
+        {x=787,m=2655,a=1222,s=2876}
+        {x=1679,m=44,a=2067,s=496}
+        {x=2036,m=264,a=79,s=2244}
+        {x=2461,m=1339,a=466,s=291}
+        {x=2127,m=1623,a=2188,s=1013}
+        `,
+        expected: 167409079868000,
+      },
     ],
     solution: part2,
   },
