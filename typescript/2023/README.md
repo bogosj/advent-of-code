0299987e6948a08208f28ac5d2422090c0d58c1e
James Bogosian <bogosj@gmail.com>
2023-12-23 08:59:07 -0500

    Day 21 part 1 solved.

--- typescript/2023/README.md
+++ typescript/2023/README.md
@@ -31,7 +31,7 @@
 [![Day](https://badgen.net/badge/18/%E2%98%85%E2%98%85/green)](src/day18)
 [![Day](https://badgen.net/badge/19/%E2%98%85%E2%98%85/green)](src/day19)
 [![Day](https://badgen.net/badge/20/%E2%98%85%E2%98%86/yellow)](src/day20)
-![Day](https://badgen.net/badge/21/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/21/%E2%98%85%E2%98%86/yellow)](src/day21)
 ![Day](https://badgen.net/badge/22/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/23/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/24/%E2%98%86%E2%98%86/gray)
@@ -209,9 +209,9 @@ Both parts: 14.76ms
 
 ```
 Day 21
-Time part 1: -
+Time part 1: 49.581ms
 Time part 2: -
-Both parts: -
+Both parts: 49.581ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 39/50
-Total time: 75982.972ms
+Total stars: 40/50
+Total time: 76032.553ms
 ```
 
 <!--/RESULTS-->
--- typescript/2023/src/day21/index.ts
+++ typescript/2023/src/day21/index.ts
@@ -1,11 +1,81 @@
 import run from "aocrunner";
 
-const parseInput = (rawInput: string) => rawInput;
+enum MapTile {
+  GardenPlot = '.',
+  Rock = '#',
+  Start = 'S'
+}
+
+type Point = {
+  x: number,
+  y: number
+}
+
+const directionDelta = {
+  'U': { x: 0, y: -1 },
+  'D': { x: 0, y: 1 },
+  'L': { x: -1, y: 0 },
+  'R': { x: 1, y: 0 }
+}
+
+const parseInput = (rawInput: string): string[][] => {
+  return rawInput.split('\n').map(line => line.split(''));
+};
+
+const findStart = (garden: string[][]): Point[] => {
+  for (let y = 0; y < garden.length; y++) {
+    for (let x = 0; x < garden[0].length; x++) {
+      if (garden[y][x] == 'S') {
+        return [{ x: x, y: y }]
+      }
+    }
+  }
+}
+
+const safePoint = (garden: string[][], point: Point): boolean => {
+  if (
+    point.y >= 0 &&
+    point.y < garden.length &&
+    point.x >= 0 &&
+    point.x < garden[0].length
+  ) {
+    return garden[point.y][point.x] != '#';
+  } else {
+    return false
+  }
+}
+
+const walk = (garden: string[][], steps: number): number => {
+  let validPoints = findStart(garden);
+  for (let step = 0; step < steps; step++) {
+    let nextValidPoints = [];
+    while (validPoints.length > 0) {
+      const nextPoint = validPoints.shift();
+      Object.values(directionDelta).forEach((delta) => {
+        const p = {
+          y: nextPoint.y + delta.y,
+          x: nextPoint.x + delta.x
+        };
+        if (safePoint(garden, p)) {
+          nextValidPoints.push(`${p.y}|${p.x}`);
+        }
+      });
+    }
+    validPoints = [...new Set(nextValidPoints)].map(s => {
+      const t = s.split('|');
+      return { y: parseInt(t[0], 10), x: parseInt(t[1], 10) }
+    });
+  }
+  return validPoints.length;
+};
 
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput);
-
-  return;
+  if (input.length == 11) { // test case
+    return walk(input, 6);
+  } else {
+    return walk(input, 64);
+  }
 };
 
 const part2 = (rawInput: string) => {
@@ -17,10 +87,22 @@ const part2 = (rawInput: string) => {
 run({
   part1: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `
+        ...........
+        .....###.#.
+        .###.##..#.
+        ..#.#...#..
+        ....#.#....
+        .##..S####.
+        .##..#...#.
+        .......##..
+        .##.#.####.
+        .##..##.##.
+        ...........
+        `,
+        expected: 16,
+      },
     ],
     solution: part1,
   },
