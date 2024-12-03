2ab2b0ab7656006c24c139688a3a3a58fb595605
James Bogosian <bogosj@gmail.com>
2024-12-03 03:14:37 +0000

    Day 2 part 2 done

--- typescript/aoc2024/src/day02/index.ts
+++ typescript/aoc2024/src/day02/index.ts
@@ -14,22 +14,26 @@ const inOrder = (l: number[]): boolean => {
   return false;
 };
 
+const isSafe = (vals: number[]): number => {
+  if (!inOrder(vals)) {
+    return 0;
+  }
+  for (let i = 0; i < vals.length - 1; i++) {
+    const delta = Math.abs(vals[i] - vals[i + 1]);
+    if (delta < 1 || delta > 3) {
+      return 0;
+    }
+  }
+  return 1;
+}
+
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput);
 
   let safeCount = 0;
   input.split('\n').forEach(line => {
     const vals = line.split(' ').map(Number);
-    if (!inOrder(vals)) {
-      return;
-    }
-    for (let i = 0; i < vals.length - 1; i++) {
-      const delta = Math.abs(vals[i] - vals[i + 1]);
-      if (delta < 1 || delta > 3) {
-        return;
-      }
-    }
-    safeCount++;
+    safeCount += isSafe(vals);
   });
 
   return safeCount;
@@ -38,7 +42,21 @@ const part1 = (rawInput: string) => {
 const part2 = (rawInput: string) => {
   const input = parseInput(rawInput);
 
-  return;
+  let safeCount = 0;
+  input.split('\n').forEach(line => {
+    const vals = line.split(' ').map(Number);
+    for (let i = 0; i < vals.length; i++) {
+      const newVals = [...vals];
+      newVals.splice(i, 1);
+      const innerSafe = isSafe(newVals);
+      if (innerSafe > 0) {
+        safeCount += innerSafe;
+        return;
+      }
+    }
+  });
+
+  return safeCount;
 };
 
 run({
@@ -58,10 +76,15 @@ run({
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9`,
+        expected: 4,
+      },
     ],
     solution: part2,
   },
